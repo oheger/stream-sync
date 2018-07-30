c7e943e2daab34182e2c1900b4ad0babbc02b551
oheger <oliver.heger@oliver-heger.de>
2018-07-30 22:10:40 +0200

    Added FolderSortStage class.
    This is a GraphStage that makes sure that the content of a directory
    is sorted alphabetically.

---
+++ src/main/scala/com/github/sync/FolderSortStage.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+
+object FolderSortStage {
+
+  /**
+    * Internally used class for keeping track on the state of the stage.
+    *
+    * This is used to figure out when a new folder starts and when sorted
+    * elements need to be pushed downstream.
+    *
+    * @param currentFolder the URI of the folder currently processed
+    * @param elements      the elements encountered for this folder
+    */
+  private case class ProcessingState(currentFolder: String,
+                                     elements: List[FsElement])
+
+  /** Constant for the URI of the root folder. */
+  private val RootFolder = ""
+
+  /**
+    * Sorts a list of elements.
+    *
+    * @param elements the list to be sorted
+    * @return the sorted list
+    */
+  private def sortElements(elements: List[FsElement]): List[FsElement] =
+    elements.sortWith(_.relativeUri < _.relativeUri)
+
+  /**
+    * Obtains the parent directory of the given element. If the element belongs
+    * to the root folder, an empty string is returned.
+    *
+    * @param elem the element
+    * @return the parent directory
+    */
+  private def parentFolder(elem: FsElement): String = {
+    val index = elem.relativeUri.lastIndexOf('/')
+    if (index > 0) elem.relativeUri.substring(0, index)
+    else RootFolder
+  }
+
+  /**
+    * The state transition function. This function is invoked for each element
+    * encountered during stream processing. It updates the current processing
+    * state and also returns a sequence of elements to be pushed downstream.
+    *
+    * @param state the current state
+    * @param elem  the next element
+    * @return a tuple with the next state and a sequence of elements to push
+    */
+  private def updateState(state: ProcessingState, elem: FsElement):
+  (ProcessingState, List[FsElement]) = {
+    val folder = parentFolder(elem)
+    if (folder == state.currentFolder)
+      (state.copy(elements = elem :: state.elements), Nil)
+    else (ProcessingState(folder, List(elem)), sortElements(state.elements))
+  }
+}
+
+/**
+  * A stage providing a default sorting function for the elements contained in
+  * a folder.
+  *
+  * The sync algorithm requires that the elements contained in a folder are
+  * processed in a defined alphabetical order. This graph stage applies this
+  * order to the current flow. So, sources do not have to implement sorting on
+  * their own.
+  *
+  * For this to work, this stage requires that all elements contained in a
+  * folder are output in a series. This can be achieved for instance if the
+  * source does breadth first traversal.
+  */
+class FolderSortStage extends GraphStage[FlowShape[FsElement, FsElement]] {
+  val in: Inlet[FsElement] = Inlet[FsElement]("FolderSortStage.in")
+  val out: Outlet[FsElement] = Outlet[FsElement]("FolderSortStage.out")
+
+  override val shape: FlowShape[FsElement, FsElement] = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+
+      import FolderSortStage._
+
+      var state = ProcessingState(RootFolder, Nil)
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val elem = grab(in)
+          val (next, elemsToPush) = updateState(state, elem)
+          state = next
+          if (elemsToPush.nonEmpty)
+            emitMultiple(out, elemsToPush)
+          else pull(in)
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          emitMultiple(out, sortElements(state.elements))
+          complete(out)
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
+}
---
+++ src/test/scala/com/github/sync/FolderSortStageSpec.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.testkit.TestKit
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+  * Test class for ''FolderSortStage''.
+  */
+class FolderSortStageSpec(testSystem: ActorSystem) extends TestKit(testSystem) with FlatSpecLike
+  with BeforeAndAfterAll with Matchers {
+  def this() = this(ActorSystem("FolderSortStageSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  /**
+    * Creates an ''FsElement'' with the given URI. Other attributes are
+    * irrelevant for the stage under test.
+    *
+    * @param uri the URI
+    * @return the element with this URI
+    */
+  private def createElem(uri: String): FsElement = FsFolder(uri, 1)
+
+  /**
+    * Generates a test ''FsElement'' that represents a file in a directory.
+    * From the indices provided a canonical URI is generated.
+    *
+    * @param dirIdx  the index of the directory
+    * @param fileIdx the index of the file in this directory
+    * @return the generated element
+    */
+  private def createFile(dirIdx: Int, fileIdx: Int): FsElement =
+    createElem(s"dir$dirIdx/file$fileIdx.txt")
+
+  /**
+    * Runs a stream with the given source over the test stage and collects the
+    * output.
+    *
+    * @param source the source of the stream
+    * @return the output produced by the stream
+    */
+  private def runStage(source: Source[FsElement, Any]): Seq[FsElement] = {
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    val sink = Sink.fold[List[FsElement], FsElement](List.empty) { (lst, e) => e :: lst }
+    val futStream = source.via(new FolderSortStage)
+      .runWith(sink)
+    Await.result(futStream, 3.seconds).reverse
+  }
+
+  "A FolderSortStage" should "handle an empty source" in {
+    val srcEmpty = Source.empty[FsElement]
+
+    val result = runStage(srcEmpty)
+    result should have size 0
+  }
+
+  it should "order the elements in a folder" in {
+    val elements = List(createElem("file3"), createElem("file1"),
+      createElem("file2"))
+    val sortedElements = List(createElem("file1"), createElem("file2"),
+      createElem("file3"))
+
+    val result = runStage(Source(elements))
+    result should contain theSameElementsInOrderAs sortedElements
+  }
+
+  it should "deal with elements in multiple folders" in {
+    val rootElem = createElem("root.txt")
+    val subElem1 = createElem("dir3/sub/fileA.mp3")
+    val subElem2 = createElem("dir3/sub/fileB.mp3")
+    val elements = List(rootElem, createFile(1, 3),
+      createFile(1, 2), createFile(1, 1),
+      createFile(2, 2), createFile(2, 1),
+      createFile(2, 3), createFile(3, 3),
+      createFile(3, 1), createFile(3, 2),
+      subElem2, subElem1)
+    val sortedElements = List(rootElem, createFile(1, 1),
+      createFile(1, 2), createFile(1, 3),
+      createFile(2, 1), createFile(2, 2),
+      createFile(2, 3), createFile(3, 1),
+      createFile(3, 2), createFile(3, 3),
+      subElem1, subElem2)
+
+    val result = runStage(Source(elements))
+    result should contain theSameElementsInOrderAs sortedElements
+  }
+
+  it should "handle an empty root directory correctly" in {
+    val elements = List(createFile(1, 2), createFile(1, 1),
+      createFile(2, 1), createFile(3, 1),
+      createFile(3, 3), createFile(3, 2))
+    val sortedElements = List(createFile(1, 1), createFile(1, 2),
+      createFile(2, 1), createFile(3, 1),
+      createFile(3, 2), createFile(3, 3))
+
+    val result = runStage(Source(elements))
+    result should contain theSameElementsInOrderAs sortedElements
+  }
+}
