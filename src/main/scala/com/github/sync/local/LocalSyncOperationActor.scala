08abafc55242c0628f3b7ec60ccd9f2727fc2710
oheger <oliver.heger@oliver-heger.de>
2018-08-16 22:12:25 +0200

    Added LocalSyncOperationActor class.
    This is an actor class that interprets SyncOperation objects on the
    local file system.

---
+++ src/main/scala/com/github/sync/local/LocalSyncOperationActor.scala
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.local
+
+import java.nio.file.attribute.FileTime
+import java.nio.file.{Files, Path}
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.FileIO
+import com.github.sync._
+
+import scala.util.{Failure, Success}
+
+/**
+  * An actor implementation that interprets ''SyncOperation'' objects by
+  * applying the corresponding changes to the local file system.
+  *
+  * The actor is initialized with the root paths of both the source and the
+  * destination structure. It is then sent the single ''SyncOperation'' objects
+  * as messages. The operations are executed, and - in case of success - sent
+  * back to the caller.
+  *
+  * @param srcPath                the path to the source structure
+  * @param dstPath                the path to the destination structure
+  * @param blockingDispatcherName name of a dispatcher for blocking operations
+  */
+class LocalSyncOperationActor(srcPath: Path, dstPath: Path, blockingDispatcherName: String)
+  extends Actor with ActorLogging {
+  /** Child actor responsible for executing sync operations. */
+  private var executorActor: ActorRef = _
+
+  override def preStart(): Unit = {
+    val sourceResolver = new LocalUriResolver(srcPath)
+    val destinationResolver = new LocalUriResolver(dstPath)
+    executorActor = context.actorOf(Props(classOf[OperationExecutorActor], sourceResolver,
+      destinationResolver).withDispatcher(blockingDispatcherName))
+  }
+
+  override def receive: Receive = {
+    case op: SyncOperation =>
+      executorActor forward op
+      log.info("OP {}", op)
+  }
+}
+
+/**
+  * An internally used actor that is responsible for the execution of
+  * operations that might fail.
+  *
+  * This additional level of indirection is added to implement supervision
+  * accordingly. [[LocalSyncOperationActor]] creates this actor as child and
+  * delegates dangerous operations to it. When an operation fails, the actor is
+  * restarted (by the default supervision strategy).
+  *
+  * @param sourceResolver      the resolver for the source structure
+  * @param destinationResolver the resolver for the destination structure
+  */
+class OperationExecutorActor(sourceResolver: LocalUriResolver,
+                             destinationResolver: LocalUriResolver) extends Actor with
+  ActorLogging {
+  /** The object to materialize streams. */
+  private implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  import context.dispatcher
+
+  override def receive: Receive = {
+    case op@SyncOperation(file: FsFile, action)
+      if action == ActionCreate || action == ActionOverride =>
+      val source = FileIO.fromPath(resolveInSource(file))
+      val destPath = resolveInDestination(file)
+      val sink = FileIO.toPath(destPath)
+      val client = sender()
+      source.runWith(sink) map { _ =>
+        Files.setLastModifiedTime(destPath, FileTime from file.lastModified)
+      } onComplete {
+        case Success(_) =>
+          client ! op
+        case Failure(exception) =>
+          log.error("Failed copy operation {}!", exception)
+      }
+
+    case op@SyncOperation(element, ActionCreate) =>
+      Files.createDirectory(resolveInDestination(element))
+      sender ! op
+
+    case op@SyncOperation(element, ActionRemove) =>
+      Files delete resolveInDestination(element)
+      sender ! op
+  }
+
+  /**
+    * Convenience method to resolve the given element in the source
+    * structure.
+    *
+    * @param element the element to be resolved
+    * @return the source path for this element
+    */
+  private def resolveInSource(element: FsElement): Path =
+    resolveElement(sourceResolver, element)
+
+  /**
+    * Convenience method to resolve the given element in the destination
+    * structure.
+    *
+    * @param element the element to be resolved
+    * @return the destination path for this element
+    */
+  private def resolveInDestination(element: FsElement): Path =
+    resolveElement(destinationResolver, element)
+
+  /**
+    * Resolves the given element using the specified resolver. Note that the
+    * ''Try'' instance from the [[LocalUriResolver]] is directly queried via
+    * ''get()''. If it failed, this will cause an exception causing the current
+    * operation to fail and this actor to be restarted.
+    *
+    * @param resolver the resolver
+    * @param element  the element to be resolved
+    * @return the destination path for this element
+    */
+  private def resolveElement(resolver: LocalUriResolver, element: FsElement): Path =
+    resolver.resolve(element).get
+}
---
+++ src/test/scala/com/github/sync/local/LocalSyncOperationActorSpec.scala
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.local
+
+import java.nio.file.attribute.FileTime
+import java.nio.file.{Files, Path, Paths}
+import java.time.Instant
+
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor.{Actor, ActorRef, ActorSystem, OneForOneStrategy, Props, SupervisorStrategy}
+import akka.testkit.{ImplicitSender, TestKit}
+import com.github.sync._
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.annotation.tailrec
+import scala.util.Random
+
+/**
+  * Test class for ''FsSyncOperationActor''.
+  */
+class LocalSyncOperationActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper {
+  def this() = this(ActorSystem("FsSyncOperationActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    tearDownTestFile()
+  }
+
+  /**
+    * Convenience function to create a file from a path.
+    *
+    * @param path the path
+    * @return the file object
+    */
+  private def createFile(path: Path): FsFile =
+    FsFile(path.getFileName.toString, 1, Instant.parse("2018-08-18T19:08:41.00Z"), 42L)
+
+  "A LocalSyncOperationActor" should "create a folder in the destination structure" in {
+    val FolderName = "newTestFolder"
+    val op = SyncOperation(FsFolder(FolderName, 1), ActionCreate)
+    val helper = new LocalActorTestHelper
+
+    val folderPath = helper.sendOperationAndExpectResponse(op)
+      .destinationPath(FolderName)
+    Files.exists(folderPath) shouldBe true
+    Files.isDirectory(folderPath) shouldBe true
+  }
+
+  it should "handle errors when creating a folder" in {
+    val op1 = SyncOperation(FsFolder("non/existing/folder/path", 1), ActionCreate)
+    val op2 = SyncOperation(FsFolder("nextAttempt", 1), ActionCreate)
+    val helper = new LocalActorTestHelper
+
+    helper.sendOperation(op1)
+      .sendOperationAndExpectResponse(op2)
+  }
+
+  it should "remove a folder from the destination structure" in {
+    val FolderName = "toBeRemoved"
+    val op = SyncOperation(FsFolder(FolderName, 1), ActionRemove)
+    val helper = new LocalActorTestHelper
+    val path = helper.destinationPath(FolderName)
+    Files createDirectory path
+
+    helper.sendOperationAndExpectResponse(op)
+    Files exists path shouldBe false
+  }
+
+  it should "remove a file from the destination structure" in {
+    val helper = new LocalActorTestHelper
+    val path = writeFileContent(helper.destinationPath("fileToBeRemoved.tmp"),
+      FileTestHelper.TestData)
+    val op = SyncOperation(createFile(path), ActionRemove)
+
+    helper.sendOperationAndExpectResponse(op)
+    Files exists path shouldBe false
+  }
+
+  it should "handle errors when removing a folder" in {
+    val op1 = SyncOperation(FsFolder("nonExistingFolder", 1), ActionRemove)
+    val op2 = SyncOperation(FsFolder("anotherFolder", 1), ActionCreate)
+    val helper = new LocalActorTestHelper
+
+    helper.sendOperation(op1)
+      .sendOperationAndExpectResponse(op2)
+  }
+
+  it should "create a file in the destination structure" in {
+    val FileName = "copy.dat"
+    val helper = new LocalActorTestHelper
+    val sourcePath = writeFileContent(helper.sourcePath(FileName), FileTestHelper.TestData)
+    val file = createFile(sourcePath)
+    val op = SyncOperation(file, ActionCreate)
+
+    val destPath = helper.sendOperationAndExpectResponse(op)
+      .destinationPath(FileName)
+    readDataFile(destPath) should be(FileTestHelper.TestData)
+    val timeDest = Files getLastModifiedTime destPath
+    timeDest should be(FileTime.from(file.lastModified))
+  }
+
+  it should "override a file in the destination structure" in {
+    val FileName = "override.dat"
+    val helper = new LocalActorTestHelper
+    val sourcePath = writeFileContent(helper.sourcePath(FileName), FileTestHelper.TestData)
+    val destPath = writeFileContent(helper.destinationPath(FileName), "some old data")
+    val op = SyncOperation(createFile(sourcePath), ActionCreate)
+
+    helper.sendOperationAndExpectResponse(op)
+    readDataFile(destPath) should be(FileTestHelper.TestData)
+  }
+
+  it should "handle errors in the source when copying a file" in {
+    val sourcePath = Paths get "nonExistingFile.xxx"
+    val op1 = SyncOperation(createFile(sourcePath), ActionCreate)
+    val op2 = SyncOperation(FsFolder("afterFailedCopy", 1), ActionCreate)
+    val helper = new LocalActorTestHelper
+
+    helper.sendOperation(op1)
+      .sendOperationAndExpectResponse(op2)
+  }
+
+  it should "handle errors in the sink when copying a file" in {
+    val FileName = "deep/toCopy.txt"
+    val helper = new LocalActorTestHelper
+    val sourcePath = helper.sourcePath(FileName)
+    Files createDirectory sourcePath.getParent
+    writeFileContent(sourcePath, FileTestHelper.TestData)
+    val op1 = SyncOperation(createFile(sourcePath), ActionCreate)
+    val op2 = SyncOperation(FsFolder("afterFailedSinkCopy", 1), ActionCreate)
+
+    helper.sendOperation(op1)
+      .sendOperationAndExpectResponse(op2)
+  }
+
+  /**
+    * Test helper class managing a test actor and some dependencies.
+    */
+  private class LocalActorTestHelper {
+    /** Object for generating random numbers. */
+    private val random = new Random
+
+    /** The path for the source structure. */
+    private val sourcePath = createDirectory()
+
+    /** The path for the destination structure. */
+    private val destinationPath = createDirectory()
+
+    /** The test actor instance. */
+    private val syncActor = createSyncActor()
+
+    /**
+      * Sends the given sync operation to the test actor.
+      *
+      * @param op the operation to send
+      * @return this test helper
+      */
+    def sendOperation(op: SyncOperation): LocalActorTestHelper = {
+      syncActor ! op
+      this
+    }
+
+    /**
+      * Sends the given sync operation to the test actor and expects the
+      * actor ACKs this by replying with the same operation.
+      *
+      * @param op the operation to send
+      * @return this test helper
+      */
+    def sendOperationAndExpectResponse(op: SyncOperation): LocalActorTestHelper = {
+      sendOperation(op)
+      expectMsg(op)
+      this
+    }
+
+    /**
+      * Resolves the given path name against the test destination directory.
+      * This can be used to check whether the test actor has created corrected
+      * elements.
+      *
+      * @param name the path name
+      * @return the resolved path in the destination folder
+      */
+    def destinationPath(name: String): Path = destinationPath resolve name
+
+    /**
+      * Resolves the given path name against the test source directory. This
+      * can be used to create source files for copy operations.
+      *
+      * @param name the path name
+      * @return the resolved path in the source folder
+      */
+    def sourcePath(name: String): Path = sourcePath resolve name
+
+    /**
+      * Creates the test actor instance.
+      *
+      * @return the test actor
+      */
+    private def createSyncActor(): ActorRef =
+      system.actorOf(Props(classOf[SupervisorActor], sourcePath, destinationPath,
+        "blocking-dispatcher"))
+
+    /**
+      * Creates a directory in the temporary folder with a unique name.
+      *
+      * @return the path to the directory
+      */
+    private def createDirectory(): Path = {
+      val path = uniqueFileName()
+      Files.createDirectory(path)
+    }
+
+    /**
+      * Generates a unique file name in the test directory.
+      *
+      * @return the unique file path
+      */
+    @tailrec private def uniqueFileName(): Path = {
+      val name = "test" + random.nextLong()
+      val path = testDirectory resolve name
+      if (Files.exists(path)) uniqueFileName()
+      else path
+    }
+  }
+
+}
+
+/**
+  * An actor that implements supervision on a test instance.
+  *
+  * This is needed to check whether exceptions are correctly handled. This
+  * actor creates a [[LocalSyncOperationActor]] as child and sets a supervision
+  * strategy the stops this child actor for all exceptions.
+  *
+  * @param srcPath                the source path
+  * @param dstPath                the destination path
+  * @param blockingDispatcherName name of the blocking dispatcher
+  */
+class SupervisorActor(srcPath: Path, dstPath: Path, blockingDispatcherName: String) extends Actor {
+  /** Reference to the test instance. */
+  private var syncActor: ActorRef = _
+
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _ => Stop
+  }
+
+  override def preStart(): Unit = {
+    syncActor = context.actorOf(Props(classOf[LocalSyncOperationActor], srcPath, dstPath,
+      blockingDispatcherName))
+  }
+
+  override def receive: Receive = {
+    case op: SyncOperation =>
+      syncActor forward op
+  }
+}
