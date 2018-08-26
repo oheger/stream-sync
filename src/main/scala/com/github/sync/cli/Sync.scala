763166cba8bfba891d0804dc0914944c021da0a9
oheger <oliver.heger@oliver-heger.de>
2018-08-26 21:44:19 +0200

    Sync main program now uses the manager classes.
    The main program was restructured to run a full sync process with
    parsing of parameters and filter expressions before eecuting the sync
    stream.

--- src/main/scala/com/github/sync/cli/Sync.scala
+++ src/main/scala/com/github/sync/cli/Sync.scala
@@ -16,18 +16,20 @@
 
 package com.github.sync.cli
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
+import akka.NotUsed
 import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, ClosedShape, Supervision}
 import akka.util.Timeout
+import com.github.sync.cli.FilterManager.SyncFilterData
 import com.github.sync.local.{LocalFsElementSource, LocalSyncOperationActor}
 import com.github.sync.{FolderSortStage, SyncOperation, SyncStage}
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 /**
@@ -37,23 +39,47 @@ import scala.util.{Failure, Success}
   */
 object Sync {
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      println("Usage: Sync <source> <dest>")
-      System.exit(1)
-    }
-    val sourcePath = Paths get args(0)
-    val destPath = Paths get args(1)
-
     implicit val system: ActorSystem = ActorSystem("stream-sync")
+    implicit val ec: ExecutionContext = system.dispatcher
+
+    val futSync = for {argsMap <- ParameterManager.parseParameters(args)
+                       syncUriData <- ParameterManager.extractSyncUris(argsMap)
+                       filterData <- FilterManager.parseFilters(syncUriData._1)
+                       _ <- ParameterManager.checkParametersConsumed(filterData._1)
+                       msg <- runSync(Paths get syncUriData._2._1, Paths get syncUriData._2._2,
+                         createSyncFilter(filterData._2))
+    } yield msg
+    futSync onComplete {
+      case Success(msg) =>
+        println(msg)
+      case Failure(exception) =>
+        exception.printStackTrace()
+        println("Sync process failed!")
+    }
+    futSync onComplete (_ => system.terminate())
+  }
+
+  /**
+    * Runs the stream that represents the sync process.
+    *
+    * @param srcPath    the path to the source structure
+    * @param dstPath    the path to the destination structure
+    * @param syncFilter a filter for sync operations
+    * @param system     the actor system
+    * @return a future with a message about the outcome
+    */
+  private def runSync(srcPath: Path, dstPath: Path,
+                      syncFilter: Flow[SyncOperation, SyncOperation, Any])
+                     (implicit system: ActorSystem): Future[String] = {
     val decider: Supervision.Decider = _ => Supervision.Resume
     implicit val materializer: ActorMaterializer =
       ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
     implicit val writeTimeout: Timeout = Timeout(10.seconds)
     import system.dispatcher
     val operationActor = system.actorOf(Props(classOf[LocalSyncOperationActor],
-      sourcePath, destPath, "blocking-dispatcher"))
-    val srcSource = LocalFsElementSource(sourcePath).via(new FolderSortStage)
-    val srcDest = LocalFsElementSource(destPath).via(new FolderSortStage)
+      srcPath, dstPath, "blocking-dispatcher"))
+    val srcSource = LocalFsElementSource(srcPath).via(new FolderSortStage)
+    val srcDest = LocalFsElementSource(dstPath).via(new FolderSortStage)
     val writeStage = Flow[SyncOperation].mapAsync(1) { op =>
       val futWrite = operationActor ? op
       futWrite.mapTo[SyncOperation]
@@ -68,26 +94,37 @@ object Sync {
           val broadcast = builder.add(Broadcast[SyncOperation](2))
           srcSource ~> syncStage.in0
           srcDest ~> syncStage.in1
-          syncStage.out ~> broadcast ~> sinkTotal.in
+          syncStage.out ~> syncFilter ~> broadcast ~> sinkTotal.in
           broadcast ~> writeStage ~> sinkSuccess.in
           ClosedShape
     })
     val (futTotal, futSuccess) = g.run()
-    Future.sequence(List(futTotal, futSuccess)) onComplete { f =>
-      f match {
-        case Failure(exception) =>
-          exception.printStackTrace()
-          println("Sync failed.")
-
-        case Success(value) =>
-          val totalCount = value.head
-          val successCount = value(1)
-          if (totalCount == successCount)
-            println(s"Successfully completed all ($totalCount) sync operations.")
-          else
-            println(s"$successCount operations from $totalCount were successful.")
-      }
-      system.terminate()
-    }
+    for {totalCount <- futTotal
+         successCount <- futSuccess
+    } yield processedMessage(totalCount, successCount)
   }
+
+  /**
+    * Generates a ''Flow'' that filters out undesired sync operations based on
+    * the filter parameters provided in the command line.
+    *
+    * @param filterData data about filter conditions
+    * @return the flow to filter undesired sync operations
+    */
+  private def createSyncFilter(filterData: SyncFilterData):
+  Flow[SyncOperation, SyncOperation, NotUsed] =
+    Flow[SyncOperation].filter(op => FilterManager.applyFilter(op, filterData))
+
+  /**
+    * Generates a message about te outcome of the sync operation.
+    *
+    * @param totalCount   the total number of sync operations
+    * @param successCount the number of successful sync operations
+    * @return a message about the outcome of the sync operation
+    */
+  private def processedMessage(totalCount: Int, successCount: Int): String =
+    if (totalCount == successCount)
+      s"Successfully completed all ($totalCount) sync operations."
+    else
+      s"$successCount operations from $totalCount were successful."
 }
