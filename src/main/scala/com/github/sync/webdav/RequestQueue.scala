d6348226020faec98ef0dce62146a5ed41950d47
oheger <oliver.heger@oliver-heger.de>
2018-09-23 21:35:47 +0200

    Added RequestQueue class.
    This is a helper class to send HTTP requests via a flow representing
    the host-level Akka Http Client API. The class is very similar to the
    sample code from the Akka documentation.

---
+++ src/main/scala/com/github/sync/webdav/RequestQueue.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.webdav
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult}
+
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success, Try}
+
+/**
+  * A helper class to manage a flow for sending requests to the WebDav server.
+  *
+  * This is based on the usage example of the Akka HTTP host-level client API
+  * from the official Akka documentation.
+  *
+  * @param uri    the URI requests are to be sent to
+  * @param system the actor system
+  * @param mat    the object to materialize streams
+  */
+private class RequestQueue(uri: Uri)(implicit system: ActorSystem, mat: ActorMaterializer) {
+  /**
+    * Size of the request queue. Currently, requests are sent one after
+    * another; so there is no need to do much buffering.
+    */
+  private val QueueSize = 2
+
+  /** The flow for generating HTTP requests. */
+  val poolClientFlow = createPoolClientFlow()
+
+  import system.dispatcher
+
+  /** The queue acting as source for the stream of requests and a kill switch. */
+  val queue =
+    Source.queue[(HttpRequest, Promise[HttpResponse])](QueueSize, OverflowStrategy.dropNew)
+      .via(poolClientFlow)
+      .toMat(Sink.foreach({
+        case (Success(resp), p) => p.success(resp)
+        case (Failure(e), p) => p.failure(e)
+      }))(Keep.left)
+      .run()
+
+  /**
+    * Puts a request into the queue and returns a ''Future'' with the response
+    * returned from the server.
+    *
+    * @param request the request
+    * @return a ''Future'' with the response
+    */
+  def queueRequest(request: HttpRequest): Future[HttpResponse] = {
+    val responsePromise = Promise[HttpResponse]()
+    queue.offer(request -> responsePromise).flatMap {
+      case QueueOfferResult.Enqueued =>
+        responsePromise.future
+      case QueueOfferResult.Dropped =>
+        Future.failed(new RuntimeException("Queue overflowed."))
+      case QueueOfferResult.Failure(ex) =>
+        Future.failed(ex)
+      case QueueOfferResult.QueueClosed =>
+        Future.failed(new RuntimeException("Queue was closed."))
+    }
+  }
+
+  /**
+    * Shuts down this queue by terminating the request stream. Afterwards no
+    * requests can be sent any more.
+    */
+  def shutdown(): Unit = {
+    queue.complete()
+  }
+
+  /**
+    * Creates the flow for sending HTTP requests.
+    *
+    * @return the flow
+    */
+  private def createPoolClientFlow(): Flow[(HttpRequest, Promise[HttpResponse]),
+    (Try[HttpResponse], Promise[HttpResponse]), Http.HostConnectionPool] =
+    Http().cachedHostConnectionPool[Promise[HttpResponse]](uri.authority.host.toString(),
+      uri.authority.port)
+}
