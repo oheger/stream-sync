b6e510f4f6d508935ce9656096fe684e0bef9d7c
oheger <oliver.heger@oliver-heger.de>
2019-09-19 21:50:10 +0200

    Added a new Secret class.
    This class is used to represent any kind of protected information,
    such as passwords.

---
+++ src/main/scala/com/github/sync/crypt/Secret.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.crypt
+
+object Secret {
+  /**
+    * Creates a new instance of ''Secret'' from the given string data.
+    *
+    * @param data the data as string
+    * @return the new ''Secret'' instance
+    */
+  def apply(data: String): Secret = new Secret(data.toCharArray)
+}
+
+/**
+  * A class representing a secret piece of data, such as a password or an OAuth
+  * client secret.
+  *
+  * The main purpose of this class is to store the secret information in a
+  * different representation than a plain string and to provide it as a string
+  * on demand.
+  *
+  * @param secretRaw the secret as character array
+  */
+class Secret(secretRaw: Array[Char]) {
+  /**
+    * Returns the managed client secret as string. As the secret is sensitive
+    * information, it should not be kept in memory for a longer time as string.
+    * Therefore, the string is constructed on demand only. (In general,
+    * instances of this class should not be kept in memory for a longer time.)
+    *
+    * @return the secret as a string
+    */
+  def secret: String = new String(secretRaw)
+}
---
+++ src/test/scala/com/github/sync/crypt/SecretSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.crypt
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''Secret''.
+  */
+class SecretSpec extends FlatSpec with Matchers {
+  "A Secret" should "create the password on demand" in {
+    val secret = new Secret(Array('S', '3', 'c', 'r', '3', 'T', '!'))
+
+    secret.secret should be("S3cr3T!")
+  }
+
+  it should "not cache the password" in {
+    val secret = new Secret(Array('S', '3', 'c', 'r', '4', 'T', '?'))
+    val sec1 = secret.secret
+
+    secret.secret should not be theSameInstanceAs(sec1)
+  }
+
+  it should "support creating an instance from a string" in {
+    val s = "TopS3cr3t!"
+
+    val secret = Secret(s)
+    secret.secret should not be theSameInstanceAs(s)
+  }
+}
