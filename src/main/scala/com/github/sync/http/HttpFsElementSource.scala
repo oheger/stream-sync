5d0841a1a7e3b3db90c56fc0108d88f125f1699f
oheger <oliver.heger@oliver-heger.de>
2019-10-30 18:34:41 +0100

    Extracted HTTP source functionality to a dedicated trait.
    The trait implements the full logic of iterating through a folder
    structure on an HTTP server. Derived classes need to provide the
    correct requests to access folders and parse the responses.
    
    DavFsElementSource has been implemented in terms of this new trait.

---
+++ src/main/scala/com/github/sync/http/HttpFsElementSource.scala
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2018-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.http
+
+import akka.NotUsed
+import akka.actor.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model.HttpResponse
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, Graph, SourceShape}
+import akka.util.{ByteString, Timeout}
+import com.github.sync.SyncTypes._
+import com.github.sync.http.HttpFsElementSource.{ElemData, HttpFolder, HttpIterationState}
+import com.github.sync.util.UriEncodingHelper
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object HttpFsElementSource {
+
+  /**
+    * Data class storing additional information about a folder that is to be
+    * fetched from the Http server.
+    *
+    * @param ref the URI to reference the folder on the server
+    */
+  case class HttpFolder(ref: String) {
+    /**
+      * The normalized URI to reference the folder on the server. This URI
+      * always ends on a slash which is required by some Dav servers.
+      */
+    val normalizedRef: String = UriEncodingHelper.withTrailingSeparator(ref)
+  }
+
+  /**
+    * A class representing the state of an iteration over an HTTP server's
+    * folder structure.
+    *
+    * The class stores a bunch of parameters that are needed during the
+    * iteration. There are no real state updates.
+    *
+    * @param requestActor            the actor for sending HTTP requests
+    * @param rootUriPrefix           the common prefix of all URIs in the structure to be
+    *                                processed; the URIs generated for ''FsElement''
+    *                                objects must be relative to this URI.
+    * @param decodedRootUriPrefixLen the length of the root URI prefix
+    * @param config                  the configuration for the HTTP server
+    */
+  case class HttpIterationState[C <: HttpConfig](requestActor: ActorRef,
+                                                 rootUriPrefix: String,
+                                                 decodedRootUriPrefixLen: Int,
+                                                 config: C)
+
+  /**
+    * A data class holding information about an element that is processed.
+    *
+    * @param ref  the reference URI for this element
+    * @param elem the element itself
+    */
+  case class ElemData(ref: String, elem: FsElement)
+
+}
+
+/**
+  * A trait providing basic functionality for iterating over a folder structure
+  * stored on an HTTP server.
+  *
+  * This base trait defines an iteration function and uses default
+  * representations for elements. Derived classes have to define the requests
+  * to the single folders and convert the responses to these default
+  * representations, so they can be further processed. The idea is that this
+  * trait implements the iteration logic, while derived classes integrate a
+  * concrete protocol like WebDav or OneDrive.
+  *
+  * Note that error handling is very basic: a request that fails for whatever
+  * reason (non-success server response, I/O error, unexpected data in the
+  * response, etc.) causes the source to cancel the whole stream. This is
+  * desired because when the server has a (temporary) problem a sync process
+  * must be stopped; otherwise, it can have unexpected results.
+  *
+  * @tparam C the type of the configuration needed by this source
+  */
+trait HttpFsElementSource[C <: HttpConfig] {
+  /**
+    * Creates a ''Source'' based on this class using the specified
+    * configuration.
+    *
+    * @param config         the configuration
+    * @param sourceFactory  the factory for the element source
+    * @param requestActor   the actor for sending HTTP requests
+    * @param startFolderUri URI of a folder to start the iteration
+    * @param system         the actor system
+    * @param mat            the object to materialize streams
+    * @return the new source
+    */
+  def createSource(config: C, sourceFactory: ElementSourceFactory, requestActor: ActorRef,
+                   startFolderUri: String = "")
+                  (implicit system: ActorSystem, mat: ActorMaterializer):
+  Source[FsElement, NotUsed] = Source.fromGraph(createSourceShape(config, sourceFactory, requestActor, startFolderUri))
+
+  /**
+    * Creates the source shape for iterating over an HTTP folder structure.
+    *
+    * @param config         the configuration
+    * @param sourceFactory  the factory for the element source
+    * @param requestActor   the actor for sending HTTP requests
+    * @param startFolderUri URI of a folder to start the iteration
+    * @param system         the actor system
+    * @param mat            the object to materialize streams
+    * @return the new source
+    */
+  def createSourceShape(config: C, sourceFactory: ElementSourceFactory, requestActor: ActorRef,
+                        startFolderUri: String = "")
+                       (implicit system: ActorSystem, mat: ActorMaterializer):
+  Graph[SourceShape[FsElement], NotUsed] = {
+    implicit val ec: ExecutionContext = system.dispatcher
+    val rootUriPrefix = UriEncodingHelper.removeTrailingSeparator(config.rootUri.path.toString())
+    val rootPrefixLen = UriEncodingHelper.decode(rootUriPrefix).length
+    val state = HttpIterationState(requestActor, rootUriPrefix, rootPrefixLen, config)
+    sourceFactory.createElementSource(state, createInitialFolder(config, startFolderUri),
+      Some(completionFunc))(iterateFunc)
+  }
+
+  /**
+    * Reads the entity of the given response of a folder request completely and
+    * returns the resulting ''ByteString''.
+    *
+    * @param response the response to be read
+    * @param ec       the execution context
+    * @param mat      the object to materialize streams
+    * @return a future with the ''ByteString'' built from the response
+    */
+  protected def readResponse(response: HttpResponse)
+                            (implicit ec: ExecutionContext, mat: ActorMaterializer): Future[ByteString] = {
+    val sink = Sink.fold[ByteString, ByteString](ByteString.empty)(_ ++ _)
+    response.entity.dataBytes.runWith(sink)
+  }
+
+  /**
+    * Creates a request for the specified folder. A concrete implementation
+    * here has to generate the correct request to read the content of the
+    * folder specified.
+    *
+    * @param state      the current state
+    * @param folderData the data of the folder to be loaded
+    * @return the request to query the content of this folder
+    */
+  protected def createFolderRequest(state: HttpIterationState[C], folderData: SyncFolderData[HttpFolder]):
+  HttpRequestActor.SendRequest
+
+  /**
+    * Parses the response received for a folder request. Here a concrete
+    * implementation has to process the response from the server and extracts
+    * the elements (files and folders) that are referenced.
+    *
+    * @param state  the current state
+    * @param folder the folder whose content is to be computed
+    * @param result the result of the request for this folder
+    * @param ec     the execution context
+    * @param mat    the object to materialize streams
+    * @return a ''Future'' with the elements that have been extracted
+    */
+  protected def parseFolderResponse(state: HttpIterationState[C], folder: FsFolder)(result: HttpRequestActor.Result)
+                                   (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  Future[List[ElemData]]
+
+  /**
+    * Creates the data object for the folder to start the iteration from.
+    *
+    * @param config         the configuration
+    * @param startFolderUri the start folder URI
+    * @return the object describing the initial folder to be processed
+    */
+  private def createInitialFolder(config: C, startFolderUri: String): SyncFolderData[HttpFolder] = {
+    val rootUri = UriEncodingHelper.withTrailingSeparator(config.rootUri.toString()) +
+      UriEncodingHelper.removeLeadingSeparator(UriEncodingHelper.encodeComponents(startFolderUri))
+    SyncFolderData(FsFolder(startFolderUri, UriEncodingHelper.componentCount(startFolderUri) - 1), HttpFolder(rootUri))
+  }
+
+  /**
+    * Returns the function that iterates over the folder structure of the web
+    * dav server.
+    *
+    * @param ec  the execution context
+    * @param mat the object to materialize streams
+    * @return the iterate function
+    */
+  private def iterateFunc(implicit ec: ExecutionContext, mat: ActorMaterializer):
+  IterateFunc[HttpFolder, HttpIterationState[C]] = (state, nextFolder) => {
+    nextFolder() match {
+      case Some(folder) =>
+        (state, None, Some(futureResultFunc(state, folder)))
+      case None =>
+        (state, None, None)
+    }
+  }
+
+  /**
+    * Returns the function that retrieves the content of the current folder as
+    * a ''Future'' result.
+    *
+    * @param state         the current iteration state
+    * @param currentFolder the current folder
+    * @param ec            the execution context
+    * @param mat           the object to materialize streams
+    * @return the future with the content of the current folder
+    */
+  private def futureResultFunc(state: HttpIterationState[C], currentFolder: SyncFolderData[HttpFolder])
+                              (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  FutureResultFunc[HttpFolder, HttpIterationState[C]] = () =>
+    loadFolder(state, currentFolder) map (processFolderResult(state, currentFolder.folder, _))
+
+  /**
+    * Returns the function to be called at the end of the iteration. This
+    * function shuts down the request actor stored in the given state.
+    *
+    * @return the completion function
+    */
+  private def completionFunc: CompletionFunc[HttpIterationState[C]] = state =>
+    state.requestActor ! HttpExtensionActor.Release
+
+  /**
+    * Sends a request for the content of the specified folder and parses
+    * the response.
+    *
+    * @param state      the current iteration state
+    * @param folderData the data of the folder to be loaded
+    * @param ec         the execution context
+    * @param mat        the object to materialize streams
+    * @return a future with the parsed content of the folder
+    */
+  private def loadFolder(state: HttpIterationState[C], folderData: SyncFolderData[HttpFolder])
+                        (implicit ec: ExecutionContext, mat: ActorMaterializer): Future[List[ElemData]] = {
+    implicit val timeout: Timeout = state.config.timeout
+    val request = createFolderRequest(state, folderData)
+    HttpRequestActor
+      .sendAndProcess(state.requestActor, request)(parseFolderResponse(state, folderData.folder)).flatten
+  }
+
+  /**
+    * Handles a result of parsing the content of a folder. If the folder
+    * contains elements, they are emitted downstream, and the set of sub
+    * folders pending to be processed is updated. Otherwise, processing
+    * continues with the next folder in the set.
+    *
+    * @param elements a list of elements contained in the folder
+    */
+  private def processFolderResult(state: HttpIterationState[C], currentFolder: FsFolder, elements: List[ElemData]):
+  (HttpIterationState[C], IterateResult[HttpFolder]) = {
+    val (files, folders) = elements.foldLeft((List.empty[FsFile],
+      List.empty[SyncFolderData[HttpFolder]])) { (lists, elem) =>
+      elem.elem match {
+        case f: FsFolder =>
+          (lists._1, SyncFolderData(f, HttpFolder(elem.ref)) :: lists._2)
+        case f: FsFile =>
+          (f :: lists._1, lists._2)
+      }
+    }
+    (state, IterateResult(currentFolder, files, folders))
+  }
+
+}
--- src/main/scala/com/github/sync/webdav/DavFsElementSource.scala
+++ src/main/scala/com/github/sync/webdav/DavFsElementSource.scala
@@ -26,10 +26,11 @@ import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers._
 import akka.stream._
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.{ByteString, Timeout}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 import com.github.sync.SyncTypes._
-import com.github.sync.http.{HttpExtensionActor, HttpRequestActor}
+import com.github.sync.http.HttpFsElementSource.{ElemData, HttpFolder, HttpIterationState}
+import com.github.sync.http.{HttpFsElementSource, HttpRequestActor}
 import com.github.sync.util.UriEncodingHelper
 
 import scala.annotation.tailrec
@@ -47,54 +48,8 @@ import scala.xml._
   *
   * Results ([[FsFolder]] and [[FsFile]] elements) are already returned in the
   * correct order; so no sorting is needed as post-processing step.
-  *
-  * Note that error handling is very basic: a request that fails for whatever
-  * reason (non-success server response, I/O error, unexpected data in the
-  * response, etc.) causes the source to cancel the whole stream. This is
-  * desired because when the server has a (temporary) problem a sync process
-  * must be stopped; otherwise, it can have unexpected results.
   */
-object DavFsElementSource {
-
-  /**
-    * Data class storing additional information about a folder that is to be
-    * fetched from the WebDav server.
-    *
-    * @param ref the URI to reference the folder on the server
-    */
-  case class DavFolder(ref: String) {
-    /**
-      * The normalized URI to reference the folder on the server. This URI
-      * always ends on a slash which is required by some Dav servers.
-      */
-    val normalizedRef: String = UriEncodingHelper.withTrailingSeparator(ref)
-  }
-
-  /**
-    * A class representing the state of an iteration over a dav server's folder
-    * structure.
-    *
-    * The class stores a bunch of parameters that are needed during the
-    * iteration. There are no real state updates.
-    *
-    * @param requestActor            the actor for sending HTTP requests
-    * @param rootUriPrefix           the common prefix of all URIs in the structure to be
-    *                                processed; the URIs generated for ''FsElement''
-    *                                objects must be relative to this URI.
-    * @param decodedRootUriPrefixLen the length of the root URI prefix
-    * @param config                  the configuration for the dav server
-    */
-  case class DavIterationState(requestActor: ActorRef, rootUriPrefix: String, decodedRootUriPrefixLen: Int,
-                               config: DavConfig)
-
-  /**
-    * A data class holding information about an element that is processed.
-    *
-    * @param ref  the reference URI for this element
-    * @param elem the element itself
-    */
-  case class ElemData(ref: String, elem: FsElement)
-
+object DavFsElementSource extends HttpFsElementSource[DavConfig] {
   /** Media type of the data that is expected from the server. */
   private val MediaXML = MediaRange(MediaType.text("xml"))
 
@@ -143,102 +98,7 @@ object DavFsElementSource {
   def apply(config: DavConfig, sourceFactory: ElementSourceFactory, requestActor: ActorRef,
             startFolderUri: String = "")
            (implicit system: ActorSystem, mat: ActorMaterializer):
-  Source[FsElement, NotUsed] = Source.fromGraph(createSource(config, sourceFactory, requestActor, startFolderUri))
-
-  /**
-    * Creates the source for iterating over a dav folder structure.
-    *
-    * @param config         the configuration
-    * @param sourceFactory  the factory for the element source
-    * @param requestActor   the actor for sending HTTP requests
-    * @param startFolderUri URI of a folder to start the iteration
-    * @param system         the actor system
-    * @param mat            the object to materialize streams
-    * @return the new source
-    */
-  def createSource(config: DavConfig, sourceFactory: ElementSourceFactory, requestActor: ActorRef,
-                   startFolderUri: String = "")
-                  (implicit system: ActorSystem, mat: ActorMaterializer):
-  Graph[SourceShape[FsElement], NotUsed] = {
-    implicit val ec: ExecutionContext = system.dispatcher
-    val rootUriPrefix = UriEncodingHelper.removeTrailingSeparator(config.rootUri.path.toString())
-    val rootPrefixLen = UriEncodingHelper.decode(rootUriPrefix).length
-    val state = DavIterationState(requestActor, rootUriPrefix, rootPrefixLen, config)
-    sourceFactory.createElementSource(state, createInitialFolder(config, startFolderUri),
-      Some(completionFunc))(iterateFunc)
-  }
-
-  /**
-    * Creates the data object for the folder to start the iteration from.
-    *
-    * @param config         the configuration
-    * @param startFolderUri the start folder URI
-    * @return the object describing the initial folder to be processed
-    */
-  private def createInitialFolder(config: DavConfig, startFolderUri: String): SyncFolderData[DavFolder] = {
-    val rootUri = UriEncodingHelper.withTrailingSeparator(config.rootUri.toString()) +
-      UriEncodingHelper.removeLeadingSeparator(UriEncodingHelper.encodeComponents(startFolderUri))
-    SyncFolderData(FsFolder(startFolderUri, UriEncodingHelper.componentCount(startFolderUri) - 1), DavFolder(rootUri))
-  }
-
-  /**
-    * Returns the function that iterates over the folder structure of the web
-    * dav server.
-    *
-    * @param ec  the execution context
-    * @param mat the object to materialize streams
-    * @return the iterate function
-    */
-  private def iterateFunc(implicit ec: ExecutionContext, mat: ActorMaterializer):
-  IterateFunc[DavFolder, DavIterationState] = (state, nextFolder) => {
-    nextFolder() match {
-      case Some(folder) =>
-        (state, None, Some(futureResultFunc(state, folder)))
-      case None =>
-        (state, None, None)
-    }
-  }
-
-  /**
-    * Returns the function that retrieves the content of the current folder as
-    * a ''Future'' result.
-    *
-    * @param state         the current iteration state
-    * @param currentFolder the current folder
-    * @param ec            the execution context
-    * @param mat           the object to materialize streams
-    * @return the future with the content of the current folder
-    */
-  private def futureResultFunc(state: DavIterationState, currentFolder: SyncFolderData[DavFolder])
-                              (implicit ec: ExecutionContext, mat: ActorMaterializer):
-  FutureResultFunc[DavFolder, DavIterationState] = () =>
-    loadFolder(state, currentFolder) map (processFolderResult(state, currentFolder.folder, _))
-
-  /**
-    * Returns the function to be called at the end of the iteration. This
-    * function shuts down the request queue stored in the given state.
-    *
-    * @return the completion function
-    */
-  private def completionFunc: CompletionFunc[DavIterationState] = state =>
-    state.requestActor ! HttpExtensionActor.Release
-
-  /**
-    * Sends a request for the content of the specified folder and parses
-    * the response.
-    *
-    * @param state      the current iteration state
-    * @param folderData the data of the folder to be loaded
-    * @param ec         the execution context
-    * @param mat        the object to materialize streams
-    * @return a future with the parsed content of the folder
-    */
-  private def loadFolder(state: DavIterationState, folderData: SyncFolderData[DavFolder])
-                        (implicit ec: ExecutionContext, mat: ActorMaterializer): Future[List[ElemData]] = {
-    implicit val timeout: Timeout = state.config.timeout
-    val request = createFolderRequest(state, folderData)
-    HttpRequestActor.sendAndProcess(state.requestActor, request)(parseFolderResponse(state, folderData.folder)).flatten
-  }
+  Source[FsElement, NotUsed] = createSource(config, sourceFactory, requestActor, startFolderUri)
 
   /**
     * Parses the response received for a folder request. The data of the
@@ -252,27 +112,23 @@ object DavFsElementSource {
     * @param mat    the object to materialize streams
     * @return a ''Future'' with the elements that have been extracted
     */
-  private def parseFolderResponse(state: DavIterationState, folder: FsFolder)(result: HttpRequestActor.Result)
-                                 (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  override protected def parseFolderResponse(state: HttpIterationState[DavConfig], folder: FsFolder)
+                                            (result: HttpRequestActor.Result)
+                                            (implicit ec: ExecutionContext, mat: ActorMaterializer):
   Future[List[ElemData]] =
-    readResponse(result.response).map(elem => extractFolderElements(state, elem, folder.level + 1))
+    readResponse(result.response)
+      .map(toXml)
+      .map(elem => extractFolderElements(state, elem, folder.level + 1))
 
   /**
-    * Reads the entity of the given response of a folder request and
-    * parses it as XML document.
+    * Parses the given string to an XML document.
     *
-    * @param response the response to be read
-    * @param ec       the execution context
-    * @param mat      the object to materialize streams
-    * @return a future with the parsed XML result
+    * @param data the string
+    * @return the parsed XML document
     */
-  private def readResponse(response: HttpResponse)
-                          (implicit ec: ExecutionContext, mat: ActorMaterializer): Future[Elem] = {
-    val sink = Sink.fold[ByteString, ByteString](ByteString.empty)(_ ++ _)
-    response.entity.dataBytes.runWith(sink).map { body =>
-      val stream = new ByteArrayInputStream(body.toArray)
-      XML.load(stream)
-    }
+  private def toXml(data: ByteString): Elem = {
+    val stream = new ByteArrayInputStream(data.toArray)
+    XML.load(stream)
   }
 
   /**
@@ -284,7 +140,7 @@ object DavFsElementSource {
     * @param level the level of the elements to extract
     * @return a list with elements that have been extracted
     */
-  private def extractFolderElements(state: DavIterationState, elem: Elem, level: Int): List[ElemData] =
+  private def extractFolderElements(state: HttpIterationState[DavConfig], elem: Elem, level: Int): List[ElemData] =
     (elem \ ElemResponse).drop(1) // first element is the folder itself
       .map(node => extractFolderElement(state, node, level)).toList
 
@@ -296,7 +152,7 @@ object DavFsElementSource {
     * @param level the level for this element
     * @return the element that was extracted
     */
-  private def extractFolderElement(state: DavIterationState, node: Node, level: Int): ElemData = {
+  private def extractFolderElement(state: HttpIterationState[DavConfig], node: Node, level: Int): ElemData = {
     val ref = UriEncodingHelper.removeTrailingSeparator(elemText(node, ElemHref))
     val uri = extractElementUri(state, ref)
     val propNode = node \ ElemPropStat \ ElemProp
@@ -310,35 +166,14 @@ object DavFsElementSource {
   }
 
   /**
-    * Handles a result of parsing the content of a folder. If the folder
-    * contains elements, they are emitted downstream, and the set of sub
-    * folders pending to be processed is updated. Otherwise, processing
-    * continues with the next folder in the set.
-    *
-    * @param elements a list of elements contained in the folder
-    */
-  private def processFolderResult(state: DavIterationState, currentFolder: FsFolder, elements: List[ElemData]):
-  (DavIterationState, IterateResult[DavFolder]) = {
-    val (files, folders) = elements.foldLeft((List.empty[FsFile],
-      List.empty[SyncFolderData[DavFolder]])) { (lists, elem) =>
-      elem.elem match {
-        case f: FsFolder =>
-          (lists._1, SyncFolderData(f, DavFolder(elem.ref)) :: lists._2)
-        case f: FsFile =>
-          (f :: lists._1, lists._2)
-      }
-    }
-    (state, IterateResult(currentFolder, files, folders))
-  }
-
-  /**
     * Creates a request for the specified folder.
     *
     * @param state      the current state
     * @param folderData the data of the folder to be loaded
     * @return the request to query the content of this folder
     */
-  private def createFolderRequest(state: DavIterationState, folderData: SyncFolderData[DavFolder]):
+  override protected def createFolderRequest(state: HttpIterationState[DavConfig],
+                                             folderData: SyncFolderData[HttpFolder]):
   HttpRequestActor.SendRequest = {
     val httpRequest = HttpRequest(method = MethodPropFind, uri = folderData.data.normalizedRef,
       headers = List(HeaderAccept, HeaderDepth))
@@ -352,7 +187,7 @@ object DavFsElementSource {
     * @param ref   the reference URI of the element
     * @return the extracted element URI
     */
-  private def extractElementUri(state: DavIterationState, ref: String): String =
+  private def extractElementUri(state: HttpIterationState[DavConfig], ref: String): String =
     UriEncodingHelper.decode(ref) drop state.decodedRootUriPrefixLen
 
   /**
