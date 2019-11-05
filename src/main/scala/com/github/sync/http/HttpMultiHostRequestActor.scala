d9d705b31112434f1a0d72ec4216f16e2a5c1c53
oheger <oliver.heger@oliver-heger.de>
2019-11-05 21:54:33 +0100

    Added HttpMultiHostRequestActor.
    This is an extension of an HTTP request actor that can send requests
    to multiple hosts. This is needed for some HTTP-based protocols, for
    instance for OneDrive. Here download requests are served by a
    different host than API requests.

---
+++ src/main/scala/com/github/sync/http/HttpMultiHostRequestActor.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2018-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.http
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.http.scaladsl.model.Uri
+import com.github.sync.http.HttpRequestActor.SendRequest
+import com.github.sync.util.LRUCache
+
+object HttpMultiHostRequestActor {
+  /**
+    * Returns a ''Props'' object for creating a new instance of this actor.
+    *
+    * @param cacheSize   the size of the host cache
+    * @param clientCount the initial number of clients of this actor
+    * @return a ''Props'' for creating a new actor instance
+    */
+  def apply(cacheSize: Int, clientCount: Int): Props =
+    Props(classOf[HttpMultiHostRequestActor], cacheSize, clientCount)
+}
+
+/**
+  * An actor implementation that can send HTTP requests to multiple hosts.
+  *
+  * [[HttpRequestActor]] is configured with a single host, to which all
+  * requests are sent. This actor creates such request actors dynamically
+  * whenever a new host is encountered. This is needed by certain HTTP-based
+  * protocols, for instance if download requests are served by a different
+  * host than API requests.
+  *
+  * Request actors created by this actor are hold in a LRU cache with a
+  * configurable capacity. If an actor is removed from this cache (because the
+  * maximum capacity is reached), it is stopped.
+  *
+  * @param cacheSize   the size of the host cache
+  * @param clientCount the initial number of clients of this actor
+  */
+class HttpMultiHostRequestActor(cacheSize: Int, override val clientCount: Int) extends Actor with HttpExtensionActor
+  with ActorLogging {
+  /**
+    * Reference to the HTTP actor for executing requests. Note that this field
+    * is not used by this implementation. There is not a single managed request
+    * actor, but multiple ones.
+    */
+  override protected val httpActor: ActorRef = null
+
+  /** A cache for the request actors for specific hosts. */
+  private var requestActors = LRUCache[Uri.Authority, ActorRef](cacheSize)
+
+  override protected def customReceive: Receive = {
+    case req: SendRequest =>
+      val requestActor = fetchRequestActorForUri(req.request.uri)
+      requestActor forward req
+  }
+
+  /**
+    * @inheritdoc This implementation stops this actor, which also causes the
+    *             managed request actors (its children) to be stopped.
+    */
+  override protected def release(): Unit = {
+    context stop self
+  }
+
+  /**
+    * Returns a ''Props'' object for creating a request actor for the given
+    * URI.
+    *
+    * @param uri the target URI
+    * @return a ''Props'' for the request actor for this URI
+    */
+  private[http] def requestActorProps(uri: Uri): Props = HttpRequestActor(uri)
+
+  /**
+    * Creates a new request actor based on the given properties. (This method
+    * is mainly used for testing purposes.)
+    *
+    * @param props the ''Props'' for the new request actor
+    * @return the new request actor
+    */
+  private[http] def createRequestActor(props: Props): ActorRef = context.actorOf(props)
+
+  /**
+    * Obtains the actor to handle the request for the given URI. If an actor
+    * for the current host is not yet contained in the cache, it is created
+    * now. In any case, the LRU cache is updated accordingly.
+    *
+    * @param uri the URI of the current request
+    * @return the actor to handle this request
+    */
+  private def fetchRequestActorForUri(uri: Uri): ActorRef = {
+    val authority = uri.authority
+    if (requestActors contains authority) {
+      val actor = requestActors(authority)
+      requestActors = requestActors.put(authority -> actor) // move to front
+      actor
+    } else {
+      log.info("Creating request actor for {}.", authority)
+      val actor = createRequestActor(requestActorProps(uri))
+      val nextCache = requestActors.put(authority -> actor)
+      val removedActorKeys = requestActors.keySet -- nextCache.keySet
+      removedActorKeys.map(requestActors(_)).foreach(context.stop)
+      requestActors = nextCache
+      actor
+    }
+  }
+}
---
+++ src/test/scala/com/github/sync/http/HttpMultiHostRequestActorSpec.scala
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2018-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.http
+
+import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue, TimeUnit}
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.http.scaladsl.model.headers.Location
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.testkit.{ImplicitSender, TestKit}
+import com.github.sync.http.HttpExtensionActor.Release
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object HttpMultiHostRequestActorSpec {
+  /** A data object passed to a request. */
+  private val RequestData = new Object
+
+  /** A default path for test requests. */
+  private val RequestPath = "/test-path"
+
+  /** The capacity of the host cache for the test actor. */
+  private val CacheSize = 3
+
+  /**
+    * Generates a URI to the test host with the given index.
+    *
+    * @param idx  the index of the test host
+    * @param path the path for the request
+    * @return the URI for this test request
+    */
+  private def requestUri(idx: Int, path: String): Uri =
+    Uri(s"http://test.host$idx.org$path")
+
+  /**
+    * Generates a test request to the given test host.
+    *
+    * @param idx  the index of the test host
+    * @param path the path for the request
+    * @return the request to this host
+    */
+  private def testRequest(idx: Int, path: String = RequestPath): HttpRequestActor.SendRequest = {
+    val request = HttpRequest(uri = requestUri(idx, path))
+    HttpRequestActor.SendRequest(request, RequestData)
+  }
+
+  /**
+    * Generates a result for an expected request.
+    *
+    * @param request the expected request
+    * @return the corresponding result
+    */
+  private def testResult(request: HttpRequestActor.SendRequest): HttpRequestActor.Result = {
+    val response = HttpResponse(headers = List(Location(request.request.uri)))
+    HttpRequestActor.Result(request, response)
+  }
+
+  /**
+    * Creates a ''Props'' object for a stub request actor that can handle the
+    * given set of requests.
+    *
+    * @param requests the requests supported by the actor
+    * @return ''Props'' for creating this test actor
+    */
+  private def requestActorProps(requests: Set[HttpRequestActor.SendRequest]): Props =
+    Props(classOf[StubRequestActor], requests)
+
+  /**
+    * Creates a ''Props'' object for a stub request actor that can handle only
+    * the given single request.
+    *
+    * @param singleRequest the only supported request
+    * @return ''Props'' for creating this test actor
+    */
+  private def requestActorProps(singleRequest: HttpRequestActor.SendRequest): Props =
+    requestActorProps(Set(singleRequest))
+
+  /**
+    * A test actor that simulates a request actor.
+    *
+    * The actor is initialized with a set of expected requests. When one of
+    * these requests is received it is answered with a corresponding response;
+    * other requests are ignored. That way it can be tested whether the
+    * requests are routed to the correct request actor.
+    *
+    * @param expRequests a set with expected requests
+    */
+  class StubRequestActor(expRequests: Set[HttpRequestActor.SendRequest]) extends Actor {
+    override def receive: Receive = {
+      case req: HttpRequestActor.SendRequest if expRequests.contains(req) =>
+        sender() ! testResult(req)
+    }
+  }
+
+}
+
+/**
+  * Test class for ''HttpMultiHostRequestActor''.
+  */
+class HttpMultiHostRequestActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with ImplicitSender
+  with FlatSpecLike with BeforeAndAfterAll with Matchers {
+  def this() = this(ActorSystem("HttpMultiHostRequestActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  import HttpMultiHostRequestActorSpec._
+
+  /**
+    * Creates a test actor instance that can delegate requests to the given
+    * stub actors. This function creates an actor instance with an overridden
+    * function for the creation of sub request actors.
+    *
+    * @param stubActorProps    a mapping from URIs to stub actor props
+    * @param requestActorQueue a queue to query request actors created by the
+    *                          test actor
+    * @return the test actor instance
+    */
+  private def createMultiHostActor(stubActorProps: Map[Uri, Props],
+                                   requestActorQueue: BlockingQueue[ActorRef] = new LinkedBlockingQueue): ActorRef =
+    system.actorOf(Props(new HttpMultiHostRequestActor(CacheSize, 1) {
+      override private[http] def requestActorProps(uri: Uri) = {
+        val baseProps = super.requestActorProps(uri)
+        baseProps should be(HttpRequestActor(uri))
+        stubActorProps(uri)
+      }
+
+      /**
+        * @inheritdoc This implementation stores the new actor reference in a
+        *             queue, so that it can be obtained by test cases.
+        */
+      override private[http] def createRequestActor(props: Props): ActorRef = {
+        val actor = super.createRequestActor(props)
+        requestActorQueue offer actor
+        actor
+      }
+    }))
+
+  /**
+    * Obtains a request actor from the given queue. Handles timeouts.
+    *
+    * @param queue the queue
+    * @return the request actor
+    */
+  private def pollForRequestActor(queue: BlockingQueue[ActorRef]): ActorRef = {
+    val actor = queue.poll(3, TimeUnit.SECONDS)
+    actor should not be null
+    actor
+  }
+
+  /**
+    * Sends the given sequence of requests to the test actor and expects the
+    * corresponding responses.
+    *
+    * @param multiActor the test actor
+    * @param requests   the requests to be sent
+    */
+  private def executeRequests(multiActor: ActorRef, requests: Iterable[HttpRequestActor.SendRequest]): Unit = {
+    requests foreach { req =>
+      multiActor ! req
+      expectMsg(testResult(req))
+    }
+  }
+
+  "HttpMultiHostRequestActor" should "create correct Props" in {
+    val ClientCount = 7
+    val props = HttpMultiHostRequestActor(CacheSize, ClientCount)
+
+    props.actorClass() should be(classOf[HttpMultiHostRequestActor])
+    props.args should contain theSameElementsInOrderAs List(CacheSize, ClientCount)
+  }
+
+  it should "forward requests to sub request actors" in {
+    val requests = (1 to CacheSize) map (idx => testRequest(idx))
+    val subProps = requests.map(req => (req.request.uri, requestActorProps(req))).toMap
+    val multiActor = createMultiHostActor(subProps)
+
+    executeRequests(multiActor, requests)
+  }
+
+  it should "forward requests to the same host to the same request actor" in {
+    val req1 = testRequest(1)
+    val req2 = testRequest(1, "/anotherPath")
+    val subProps = Map(req1.request.uri -> requestActorProps(Set(req1, req2)))
+    val multiActor = createMultiHostActor(subProps)
+
+    multiActor ! req1
+    expectMsg(testResult(req1))
+    multiActor ! req2
+    expectMsg(testResult(req2))
+  }
+
+  it should "keep request actors in a LRU cache and stop old instances" in {
+    val req1a = testRequest(1)
+    val req1b = testRequest(1, "/alternative")
+    val req2 = testRequest(2)
+    val req3 = testRequest(3)
+    val req4 = testRequest(4)
+    val requests = List(req1a, req2, req1b, req3, req4)
+    val subProps = Map(req1a.request.uri -> requestActorProps(Set(req1a, req1b)),
+      req2.request.uri -> requestActorProps(req2),
+      req3.request.uri -> requestActorProps(req3),
+      req4.request.uri -> requestActorProps(req4))
+    val requestActorQueue = new LinkedBlockingQueue[ActorRef]
+    val multiActor = createMultiHostActor(subProps, requestActorQueue)
+
+    executeRequests(multiActor, requests)
+    val reqActor1 = pollForRequestActor(requestActorQueue) // first actor still alive
+    reqActor1 ! req1a
+    expectMsg(testResult(req1a))
+    val reqActor2 = pollForRequestActor(requestActorQueue) // second one should be stopped
+    watch(reqActor2)
+    expectTerminated(reqActor2)
+  }
+
+  it should "handle the Release message" in {
+    val requests = (1 to CacheSize) map (idx => testRequest(idx))
+    val subProps = requests.map(req => (req.request.uri, requestActorProps(req))).toMap
+    val requestActorQueue = new LinkedBlockingQueue[ActorRef]
+    val multiActor = createMultiHostActor(subProps, requestActorQueue)
+    executeRequests(multiActor, requests)
+    val requestActors = requests map (_ => pollForRequestActor(requestActorQueue))
+
+    multiActor ! Release
+    requestActors foreach { actor =>
+      watch(actor)
+      expectTerminated(actor)
+    }
+    watch(multiActor)
+    expectTerminated(multiActor)
+  }
+}
