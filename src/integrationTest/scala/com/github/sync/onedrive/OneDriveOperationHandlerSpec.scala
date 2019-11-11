20c1a213c61b11a5affa0af0953069a19ecdfa6a
oheger <oliver.heger@oliver-heger.de>
2019-11-11 21:50:53 +0100

    OneDriveOperationHandler now supports file uploads.
    The handler now integrates the new OneDriveUpload module to generate
    the requests to update or create a file on the server.

--- src/integrationTest/scala/com/github/sync/onedrive/OneDriveOperationHandlerSpec.scala
+++ src/integrationTest/scala/com/github/sync/onedrive/OneDriveOperationHandlerSpec.scala
@@ -20,18 +20,21 @@ import java.time.Instant
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
 import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import akka.testkit.TestKit
-import com.github.sync.SyncTypes.{ActionCreate, ActionRemove, FsFile, FsFolder, SyncOperation}
-import com.github.sync.http.{HttpBasicAuthActor, HttpRequestActor}
+import akka.util.ByteString
+import com.github.sync.SyncTypes._
+import com.github.sync.http.HttpRequestActor
 import com.github.sync.util.UriEncodingHelper
-import com.github.sync.{AsyncTestHelper, SourceFileProvider, WireMockSupport}
+import com.github.sync.{AsyncTestHelper, FileTestHelper, SourceFileProvider, WireMockSupport}
 import com.github.tomakehurst.wiremock.client.WireMock._
+import org.mockito.Mockito
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpecLike, Matchers}
 import org.scalatestplus.mockito.MockitoSugar
 
-import scala.concurrent.duration._
+import scala.concurrent.Future
 
 object OneDriveOperationHandlerSpec {
   /** The path to be synced. */
@@ -57,6 +60,7 @@ class OneDriveOperationHandlerSpec(testSystem: ActorSystem) extends TestKit(test
   }
 
   import OneDriveOperationHandlerSpec._
+  import OneDriveStubbingSupport._
   import WireMockSupport._
 
   /**
@@ -65,7 +69,7 @@ class OneDriveOperationHandlerSpec(testSystem: ActorSystem) extends TestKit(test
     * @return the test configuration
     */
   private def createConfig(): OneDriveConfig =
-    createOneDriveConfig(SyncPath).copy(uploadChunkSize = 1024)
+    createOneDriveConfig(SyncPath)
 
   /**
     * Executes a stream with the given sync operations and returns the result.
@@ -140,5 +144,87 @@ class OneDriveOperationHandlerSpec(testSystem: ActorSystem) extends TestKit(test
     syncRes should contain only op
     verify(postRequestedFor(urlPathEqualTo(path(mapFolderUri(config, parentUri))))
       .withRequestBody(equalToJson(expRequest)))
+  }
+
+  /**
+    * Checks a file upload operation with different parameters.
+    *
+    * @param uploadChunkSize the upload chunk size
+    * @param groupSize       the group size in the stream
+    * @param action          the action
+    */
+  private def checkUpload(uploadChunkSize: Int, groupSize: Int, action: SyncAction): Unit = {
+    val Content = FileTestHelper.TestData * 8
+    val SourceUri = "/source/file/data.txt"
+    val FileUri = "/upload/file/data.txt"
+    val UploadUri = "/1234567890/foo/bar.xyz"
+    val ModifiedTime = "2019-11-09T19:14:35Z"
+    val file = FsFile("/some/file.txt", 3, Instant.parse(ModifiedTime), 42)
+    val op = SyncOperation(file, action, 0, SourceUri, FileUri)
+    val config = createConfig().copy(uploadChunkSize = uploadChunkSize)
+    val UploadSessionRequest =
+      s"""
+         |{
+         |  "item": {
+         |    "@microsoft.graph.conflictBehavior": "replace",
+         |    "fileSystemInfo": {
+         |      "lastModifiedDateTime": "$ModifiedTime"
+         |    }
+         |  }
+         |}
+         |""".stripMargin
+    val UploadSessionResponse =
+      s"""
+         |{
+         |  "uploadUrl": "${serverUri(UploadUri)}",
+         |  "expirationDateTime": "2015-01-29T09:21:55.523Z"
+         |}
+         |""".stripMargin
+    val fileProvider = mock[SourceFileProvider]
+    val fileSource = Source(Content.grouped(groupSize).toList).map(ByteString(_))
+    Mockito.when(fileProvider.fileSource(SourceUri)).thenReturn(Future.successful(fileSource))
+    Mockito.when(fileProvider.fileSize(file.size)).thenReturn(Content.length)
+    stubSuccess(NoAuthFunc)
+    stubFor(post(urlPathEqualTo(path(mapElementUri(config, FileUri, prefix = PrefixItems)) + ":/createUploadSession"))
+      .withHeader("Content-Type", equalTo("application/json"))
+      .withHeader("Accept", equalTo("application/json"))
+      .withRequestBody(equalToJson(UploadSessionRequest))
+      .willReturn(aResponse().withStatus(StatusCodes.OK.intValue)
+        .withHeader("Content-Type", ContentType)
+        .withBody(UploadSessionResponse)))
+    val syncRes = runSync(List(op), config, fileProvider)
+    syncRes should contain only op
+
+    import collection.JavaConverters._
+    getAllServeEvents.asScala foreach { event =>
+      println(event.getRequest)
+    }
+    Content.grouped(uploadChunkSize)
+      .zipWithIndex
+      .foreach { t =>
+        val startRange = t._2 * uploadChunkSize
+        val endRange = math.min(startRange + uploadChunkSize - 1, Content.length - 1)
+        val rangeHeader = s"bytes $startRange-$endRange/${Content.length}"
+        println("Verify request for " + rangeHeader)
+        verify(putRequestedFor(urlPathEqualTo(UploadUri))
+          .withHeader("Content-Range", equalTo(rangeHeader))
+          .withRequestBody(equalTo(t._1)))
+      }
+  }
+
+  it should "upload a new file" in {
+    checkUpload(16384, 1024, ActionCreate)
+  }
+
+  it should "upload a modified file" in {
+    checkUpload(16384, 2048, ActionOverride)
+  }
+
+  it should "upload a file with multiple chunks if the group size fits into the chunk size" in {
+    checkUpload(2048, 1024, ActionCreate)
+  }
+
+  it should "upload a file with multiple chunks if the group size does not fit into the chunk size" in {
+    checkUpload(3333, 1024, ActionOverride)
   }
 }
--- src/main/scala/com/github/sync/onedrive/OneDriveOperationHandler.scala
+++ src/main/scala/com/github/sync/onedrive/OneDriveOperationHandler.scala
@@ -20,28 +20,53 @@ import java.util.Locale
 
 import akka.NotUsed
 import akka.actor.{ActorRef, ActorSystem}
-import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.headers.Accept
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Source}
-import akka.util.ByteString
+import akka.util.{ByteString, Timeout}
+import com.github.sync.SourceFileProvider
 import com.github.sync.SyncTypes.{FsFile, FsFolder, SyncOperation}
 import com.github.sync.http.SyncOperationRequestActor.SyncOperationRequestData
-import com.github.sync.http.{HttpOperationHandler, SyncOperationRequestActor}
+import com.github.sync.http.{HttpOperationHandler, HttpRequestActor}
 import com.github.sync.util.UriEncodingHelper
-import com.github.sync.{SourceFileProvider, SyncTypes}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object OneDriveOperationHandler {
   /** Template for a request to create a new folder. */
   private val CreateFolderTemplate =
+    """
+      |{
+      |  "name": "%s",
+      |  "folder": {},
+      |  "@microsoft.graph.conflictBehavior": "fail"
+      |}
+      |""".stripMargin
+
+  /** Template for a request to create an upload session. */
+  private val CreateUploadSessionTemplate =
     s"""
        |{
-       |  "name": "%s",
-       |  "folder": {},
-       |  "@microsoft.graph.conflictBehavior": "fail"
+       |  "item": {
+       |    "@microsoft.graph.conflictBehavior": "replace",
+       |    "fileSystemInfo": {
+       |      "lastModifiedDateTime": "%s"
+       |    }
+       |  }
        |}
        |""".stripMargin
+
+  /** Media type of the data that is expected from the server. */
+  private val MediaJson = MediaRange(MediaType.applicationWithFixedCharset("json", HttpCharsets.`UTF-8`))
+
+  /** The Accept header to be used by all requests. */
+  private val HeaderAccept = Accept(MediaJson)
+
+  /** A list with the headers for a request for an upload session. */
+  private val UploadSessionRequestHeaders = List(HeaderAccept)
 
   /**
     * Creates the ''Flow'' for applying sync operations against a OneDrive
@@ -56,7 +81,7 @@ object OneDriveOperationHandler {
     */
   def apply(config: OneDriveConfig, fileProvider: SourceFileProvider, requestActor: ActorRef)
            (implicit system: ActorSystem, mat: ActorMaterializer): Flow[SyncOperation, SyncOperation, NotUsed] = {
-    val handler = new OneDriveOperationHandler(config)
+    val handler = new OneDriveOperationHandler(config, requestActor)
     handler.webDavProcessingFlow(config, fileProvider, requestActor)
   }
 }
@@ -68,9 +93,12 @@ object OneDriveOperationHandler {
   * The class generates the requests to manipulate a OneDrive structure
   * according to a stream of ''SyncOperation'' objects.
   *
-  * @param config the configuration of the OneDrive server
+  * @param config       the configuration of the OneDrive server
+  * @param requestActor the HTTP request actor
+  * @param system       the actor system
   */
-class OneDriveOperationHandler(config: OneDriveConfig) extends HttpOperationHandler[OneDriveConfig] {
+class OneDriveOperationHandler(config: OneDriveConfig, requestActor: ActorRef)(implicit system: ActorSystem)
+  extends HttpOperationHandler[OneDriveConfig] {
 
   import OneDriveOperationHandler._
 
@@ -90,31 +118,15 @@ class OneDriveOperationHandler(config: OneDriveConfig) extends HttpOperationHand
     simpleRequest(op, request)
   }
 
-  /**
-    * Creates an object describing the request to create a new file.
-    *
-    * @param op       the sync operation
-    * @param fileSize the adjusted file size
-    * @param file     the file affected
-    * @param source   a ''Source'' with the content of the file
-    * @param ec       the execution context
-    * @param mat      the object to materialize streams
-    * @return a ''Future'' with request information
-    */
-  override protected def createNewFileRequest(op: SyncTypes.SyncOperation, file: SyncTypes.FsFile, fileSize: Long, source: Future[Source[ByteString, Any]])(implicit ec: ExecutionContext, mat: ActorMaterializer): Future[SyncOperationRequestActor.SyncOperationRequestData] = ???
+  override protected def createNewFileRequest(op: SyncOperation, file: FsFile, fileSize: Long,
+                                              source: Future[Source[ByteString, Any]])
+                                             (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  Future[SyncOperationRequestData] = createUploadRequest(op, file, fileSize, source)
 
-  /**
-    * Creates an object describing the request to replace a file on the server.
-    *
-    * @param op       the sync operation
-    * @param fileSize the adjusted file size
-    * @param file     the file affected
-    * @param source   a ''Source'' with the content of the file
-    * @param ec       the execution context
-    * @param mat      the object to materialize streams
-    * @return a ''Future'' with request information
-    */
-  override protected def createUpdateFileRequest(op: SyncTypes.SyncOperation, file: SyncTypes.FsFile, fileSize: Long, source: Future[Source[ByteString, Any]])(implicit ec: ExecutionContext, mat: ActorMaterializer): Future[SyncOperationRequestActor.SyncOperationRequestData] = ???
+  override protected def createUpdateFileRequest(op: SyncOperation, file: FsFile, fileSize: Long,
+                                                 source: Future[Source[ByteString, Any]])
+                                                (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  Future[SyncOperationRequestData] = createUploadRequest(op, file, fileSize, source)
 
   /**
     * Generates the request to delete an element. This can be used for both
@@ -126,4 +138,50 @@ class OneDriveOperationHandler(config: OneDriveConfig) extends HttpOperationHand
   private def createRemoveRequest(op: SyncOperation):
   Future[SyncOperationRequestData] =
     simpleRequest(op, HttpRequest(method = HttpMethods.DELETE, uri = config.resolveRelativeUri(op.dstUri)))
+
+  /**
+    * Generates the request to upload a file. Here multiple requests can be
+    * returned, if the file upload needs to be split into multiple chunks.
+    *
+    * @param op       the sync operation
+    * @param file     the file to be uploaded
+    * @param fileSize the total file size
+    * @param source   the source with the content of the file
+    * @param ec       the execution context
+    * @param mat      the object to materialize streams
+    * @return a ''Future'' with information about the request
+    */
+  private def createUploadRequest(op: SyncOperation, file: FsFile, fileSize: Long,
+                                  source: Future[Source[ByteString, Any]])
+                                 (implicit ec: ExecutionContext, mat: ActorMaterializer):
+  Future[SyncOperationRequestData] =
+    for {
+      uploadUri <- fetchUploadUri(op, file)
+      dataSource <- source
+    } yield OneDriveUpload.upload(config, fileSize, dataSource, uploadUri, op)
+
+  /**
+    * Obtains the URI for a file upload operation. This function sends a
+    * request to create a new upload session. From the response the upload URI
+    * can be extracted.
+    *
+    * @param op   the current sync operation
+    * @param file the file to be uploaded
+    * @param ec   the execution context
+    * @param mat  the object to materialize streams
+    * @return a ''Future'' with the upload URI
+    */
+  private def fetchUploadUri(op: SyncOperation, file: FsFile)
+                            (implicit ec: ExecutionContext, mat: ActorMaterializer): Future[String] = {
+    val body = String.format(Locale.ROOT, CreateUploadSessionTemplate, file.lastModified)
+    val request = HttpRequest(method = HttpMethods.POST, headers = UploadSessionRequestHeaders,
+      uri = config.resolveItemsUri(op.dstUri + ":/createUploadSession"),
+      entity = HttpEntity(ContentTypes.`application/json`, body))
+    implicit val timeout: Timeout = config.timeout
+    import OneDriveJsonProtocol._
+    for {
+      result <- HttpRequestActor.sendRequest(requestActor, HttpRequestActor.SendRequest(request, null))
+      sessionResponse <- Unmarshal(result.response).to[UploadSessionResponse]
+    } yield sessionResponse.uploadUrl
+  }
 }
