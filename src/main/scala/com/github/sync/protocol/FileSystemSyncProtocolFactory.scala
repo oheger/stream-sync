44cd735e6d54eca3372b3fad1478f782dc506de4
oheger <oliver.heger@oliver-heger.de>
2021-05-30 22:08:11 +0200

    Added FileSystemSyncProtocolFactory class.
    This is a factory class for creating FileSystemSyncProtocol instances.
    The factory is associated with a FileSystemProtocolCreator, which is
    responsible for creating the single components a protocol requires. It
    then assembles these components to create the final
    FileSystemSyncProtocol.

---
+++ src/main/scala/com/github/sync/protocol/FileSystemSyncProtocolFactory.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.protocol
+
+import akka.actor.typed.ActorSystem
+import akka.util.Timeout
+import com.github.cloudfiles.core.Model
+import com.github.cloudfiles.core.delegate.ExtensibleFileSystem
+import com.github.cloudfiles.core.http.factory.{HttpRequestSenderConfig, HttpRequestSenderFactory, HttpRequestSenderFactoryImpl, Spawner}
+import com.github.cloudfiles.crypt.alg.aes.Aes
+import com.github.cloudfiles.crypt.fs.resolver.CachePathComponentsResolver
+import com.github.cloudfiles.crypt.fs.{CryptConfig, CryptContentFileSystem, CryptNamesFileSystem}
+import com.github.sync.protocol.FileSystemSyncProtocolFactory.createCryptConfig
+import com.github.sync.protocol.config.StructureCryptConfig
+
+import java.security.SecureRandom
+
+object FileSystemSyncProtocolFactory {
+  /**
+    * A ''SecureRandom'' object that is used as source for randomness for all
+    * cryptographic operations.
+    */
+  private val RandomSource = new SecureRandom
+
+  /**
+    * Creates a ''CryptConfig'' with default settings and keys derived from the
+    * passed in password.
+    *
+    * @param password the password
+    * @return the ''CryptConfig'' using this password
+    */
+  private def createCryptConfig(password: String): CryptConfig = {
+    val key = Aes.keyFromString(password)
+    CryptConfig(Aes, key, key, RandomSource)
+  }
+}
+
+/**
+  * An implementation of [[SyncProtocolFactory]] that creates a protocol based
+  * on a ''FileSystem''.
+  *
+  * This class implements all the steps necessary to construct a
+  * fully-initialized [[FileSystemSyncProtocol]] implementation. This involves
+  * the creation of the single components that are needed for such a protocol.
+  * To do this in a protocol-specific manner, it delegates to a
+  * [[FileSystemProtocolCreator]] object. The components created that way are
+  * then further processed if necessary (e.g. by adding encryption support if
+  * desired) and then bundled together to a working protocol instance.
+  *
+  * @param creator           the object to create protocol-specific components
+  * @param httpSenderConfig  the config for the HTTP sender actor
+  * @param timeout           the timeout for sync operations
+  * @param spawner           the object to spawn new actors
+  * @param httpSenderFactory the factory for the HTTP sender actor
+  * @param system            the actor system
+  * @tparam ID     the type of file system IDs
+  * @tparam FILE   the type of file system files
+  * @tparam FOLDER the type of file system folders
+  * @tparam C      the type of configuration for the supported sync structure
+  */
+class FileSystemSyncProtocolFactory[ID, FILE <: Model.File[ID], FOLDER <: Model.Folder[ID],
+  C](val creator: FileSystemProtocolCreator[ID, FILE, FOLDER, C],
+     val httpSenderConfig: HttpRequestSenderConfig,
+     val timeout: Timeout,
+     spawner: Spawner,
+     httpSenderFactory: HttpRequestSenderFactory = HttpRequestSenderFactoryImpl)
+    (implicit system: ActorSystem[_]) extends SyncProtocolFactory[C] {
+  override def createProtocol(uri: String, config: C, cryptConfig: StructureCryptConfig): SyncProtocol = {
+    val fileSystem = decorateFileSystem(creator.createFileSystem(uri, config, timeout), cryptConfig)
+    val httpSender = creator.createHttpSender(spawner, httpSenderFactory, uri, config, httpSenderConfig)
+    val converter = creator.createConverter(config)
+
+    new FileSystemSyncProtocol(fileSystem, httpSender, converter)
+  }
+
+  /**
+    * Decorates the passed in extensible file system to support the desired
+    * cryptographic operations.
+    *
+    * @param fs                the original file system
+    * @param structCryptConfig the cryptography-related configuration
+    * @return the decorated file system
+    */
+  private def decorateFileSystem(fs: ExtensibleFileSystem[ID, FILE, FOLDER, Model.FolderContent[ID, FILE, FOLDER]],
+                                 structCryptConfig: StructureCryptConfig):
+  ExtensibleFileSystem[ID, FILE, FOLDER, Model.FolderContent[ID, FILE, FOLDER]] =
+    structCryptConfig.password.fold(fs) { password =>
+      val cryptConfig = createCryptConfig(password)
+      val fsc = new CryptContentFileSystem(fs, cryptConfig)
+      if (structCryptConfig.cryptNames) {
+        implicit val resolverTimeout: Timeout = timeout
+        new CryptNamesFileSystem(fsc, cryptConfig,
+          resolver = CachePathComponentsResolver(spawner, structCryptConfig.cryptCacheSize))
+      } else fsc
+    }
+}
---
+++ src/test/scala/com/github/sync/protocol/FileSystemSyncProtocolFactorySpec.scala
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.protocol
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.{ActorRef, Behavior, Props}
+import akka.util.Timeout
+import com.github.cloudfiles.core.delegate.ExtensibleFileSystem
+import com.github.cloudfiles.core.http.HttpRequestSender
+import com.github.cloudfiles.core.http.factory.{HttpRequestSenderConfig, HttpRequestSenderFactory, Spawner}
+import com.github.cloudfiles.core.{FileSystem, Model}
+import com.github.cloudfiles.crypt.alg.aes.Aes
+import com.github.cloudfiles.crypt.fs.{CryptConfig, CryptContentFileSystem, CryptNamesFileSystem}
+import com.github.sync.protocol.config.StructureCryptConfig
+import org.mockito.Mockito.when
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.duration._
+
+object FileSystemSyncProtocolFactorySpec {
+  /** The protocol-specific URI used by tests. */
+  private val ProtocolUri = "https://test.protocol.example.org"
+
+  /** The protocol-specific configuration. */
+  private val ProtocolConfig = 20210529
+
+  /** A test configuration for the HTTP sender actor. */
+  private val SenderConfig = HttpRequestSenderConfig(queueSize = 42)
+
+  /** A test password to use for encryption. */
+  private val CryptPassword = "#CryptPwd!"
+
+  /** The key for encrypt operations derived from the test password. */
+  private val CryptKey = Aes.keyFromString(CryptPassword)
+
+  /** Constant for a file system timeout. */
+  private val FsTimeout = Timeout(2.minutes)
+}
+
+/**
+  * Test class for ''FileSystemSyncProtocolFactory''.
+  */
+class FileSystemSyncProtocolFactorySpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with MockitoSugar {
+
+  import FileSystemSyncProtocolFactorySpec._
+
+  /**
+    * Checks whether the given config contains the expected values.
+    *
+    * @param config the config to be checked
+    */
+  private def checkCryptConfig(config: CryptConfig): Unit = {
+    config.algorithm should be(Aes)
+    config.keyDecrypt should be(CryptKey)
+    config.keyEncrypt should be(CryptKey)
+    config.secRandom should not be null
+  }
+
+  "FileSystemSyncProtocolFactory" should "create a correct protocol without encryption" in {
+    val cryptConfig = StructureCryptConfig(password = None, cryptNames = true, cryptCacheSize = 0)
+    val helper = new ProtocolFactoryHelper
+
+    val protocol = helper.createAndCheckProtocol(cryptConfig)
+    helper.checkFileSystem(protocol.fileSystem)
+    helper.numberOfSpawns should be(0)
+  }
+
+  it should "create a file system supporting encrypted file content" in {
+    val cryptConfig = StructureCryptConfig(password = Some(CryptPassword), cryptNames = false, cryptCacheSize = 100)
+    val helper = new ProtocolFactoryHelper
+
+    val protocol = helper.createAndCheckProtocol(cryptConfig)
+    protocol.fileSystem match {
+      case fs: CryptContentFileSystem[_, _, _] =>
+        checkCryptConfig(fs.config)
+        helper.checkFileSystem(fs.delegate)
+      case o => fail("Unexpected file system: " + o)
+    }
+    helper.numberOfSpawns should be(0)
+  }
+
+  it should "create a file system supporting both encrypted content and element names" in {
+    val cryptConfig = StructureCryptConfig(password = Some(CryptPassword), cryptNames = true, cryptCacheSize = 100)
+    val helper = new ProtocolFactoryHelper
+
+    val protocol = helper.createAndCheckProtocol(cryptConfig)
+    protocol.fileSystem match {
+      case fsn: CryptNamesFileSystem[_, _, _] =>
+        checkCryptConfig(fsn.config)
+        fsn.delegate match {
+          case fsc: CryptContentFileSystem[_, _, _] =>
+            checkCryptConfig(fsc.config)
+            helper.checkFileSystem(fsc.delegate)
+          case o => fail("Unexpected delegate of CryptNamesFileSystem: " + o)
+        }
+      case o => fail("Unexpected file system: " + o)
+    }
+    helper.numberOfSpawns should be(1)
+  }
+
+  /**
+    * A test helper class managing an object to test and its dependencies.
+    */
+  private class ProtocolFactoryHelper {
+    /** A counter for spawn operations. */
+    private val spawnCounter = new AtomicInteger
+
+    /** The spawner object. */
+    private val spawner = createSpawner()
+
+    /** Mock for the HTTP sender factory. */
+    private val senderFactory = mock[HttpRequestSenderFactory]
+
+    /** Mock for the file system returned by the creator. */
+    private val fileSystem = mock[ExtensibleFileSystem[String, Model.File[String], Model.Folder[String],
+      Model.FolderContent[String, Model.File[String], Model.Folder[String]]]]
+
+    /** Mock for the converter returned by the creator. */
+    private val converter = mock[FileSystemProtocolConverter[String, Model.File[String], Model.Folder[String]]]
+
+    /** The HTTP sender actor returned by the creator. */
+    private val httpSender = testKit.createTestProbe[HttpRequestSender.HttpCommand]().ref
+
+    /** The mock for the protocol creator. */
+    private val creator = createCreator()
+
+    /** The factory to be tested. */
+    private val factory =
+      new FileSystemSyncProtocolFactory[String, Model.File[String], Model.Folder[String], Int](creator, SenderConfig,
+        FsTimeout, spawner, senderFactory)
+
+    /**
+      * Invokes the test factory to create a protocol based on the managed mock
+      * dependencies. Checks whether the result contains the expected
+      * components, except for the file system, which may be decorated.
+      *
+      * @param cryptConfig the cryptography-related config to be passed
+      * @return the validated protocol
+      */
+    def createAndCheckProtocol(cryptConfig: StructureCryptConfig):
+    FileSystemSyncProtocol[_, _, _] =
+      factory.createProtocol(ProtocolUri, ProtocolConfig, cryptConfig) match {
+        case p: FileSystemSyncProtocol[_, _, _] =>
+          p.converter should be(converter)
+          p.httpSender should be(httpSender)
+          p
+        case o => fail("Unexpected protocol: " + o)
+      }
+
+    /**
+      * Checks whether the passed in file system is the expected base file
+      * system for the protocol created by the test factory.
+      *
+      * @param fs the actual file system to be tested
+      */
+    def checkFileSystem(fs: FileSystem[_, _, _, _]): Unit = {
+      fs should be(fileSystem)
+    }
+
+    /**
+      * Returns the number of actors that have been created via the test
+      * ''Spawner''.
+      *
+      * @return the number of created actors
+      */
+    def numberOfSpawns: Int = spawnCounter.get()
+
+    /**
+      * Creates a new ''Spawner'' instance. This is actually a functional
+      * implementation based on the test kit, which also counts invocations.
+      * This is done to test whether a resolver for encrypted paths is created.
+      * (Unfortunately, there is no access to the actor and its parameters that
+      * is created.)
+      *
+      * @return the ''Spawner'' instance
+      */
+    private def createSpawner(): Spawner = new Spawner {
+      override def spawn[T](behavior: Behavior[T], optName: Option[String], props: Props): ActorRef[T] = {
+        spawnCounter.incrementAndGet()
+        optName should be(None)
+        testKit.spawn(behavior)
+      }
+    }
+
+    /**
+      * Creates the mock for the protocol creator and prepares it to handle
+      * the expected invocations.
+      *
+      * @return the ''FileSystemProtocolCreator'' mock
+      */
+    private def createCreator(): FileSystemProtocolCreator[String, Model.File[String], Model.Folder[String], Int] = {
+      val c = mock[FileSystemProtocolCreator[String, Model.File[String], Model.Folder[String], Int]]
+      when(c.createFileSystem(ProtocolUri, ProtocolConfig, FsTimeout)).thenReturn(fileSystem)
+      when(c.createHttpSender(spawner, senderFactory, ProtocolUri, ProtocolConfig, SenderConfig))
+        .thenReturn(httpSender)
+      when(c.createConverter(ProtocolConfig)).thenReturn(converter)
+      c
+    }
+  }
+}
