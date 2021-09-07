a6180aaec22a103863e14dfd8e89dbca3fc0722e
oheger <oliver.heger@oliver-heger.de>
2021-09-07 21:32:14 +0200

    Added GoogleDriveProtocolFactory class.
    This class is the main factory for creating the components to access
    data on GoogleDrive from the CLI.

---
+++ src/main/scala/com/github/sync/protocol/gdrive/GoogleDriveProtocolFactory.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.protocol.gdrive
+
+import akka.actor.typed.ActorSystem
+import akka.util.Timeout
+import com.github.cloudfiles.core.http.factory.{HttpRequestSenderConfig, Spawner}
+import com.github.cloudfiles.gdrive.GoogleDriveModel
+import com.github.sync.protocol.FileSystemSyncProtocolFactory
+import com.github.sync.protocol.config.GoogleDriveStructureConfig
+
+/**
+  * A concrete ''SyncProtocolFactory'' for creating a protocol to access a
+  * file system via the OneDrive protocol.
+  *
+  * @param config           the protocol-specific configuration
+  * @param httpSenderConfig the config for the HTTP sender actor
+  * @param timeout          the timeout for sync operations
+  * @param spawner          the object to spawn new actors
+  * @param system           the actor system
+  */
+class GoogleDriveProtocolFactory(config: GoogleDriveStructureConfig,
+                                 httpSenderConfig: HttpRequestSenderConfig,
+                                 timeout: Timeout,
+                                 spawner: Spawner)
+                                (implicit system: ActorSystem[_])
+  extends FileSystemSyncProtocolFactory[String, GoogleDriveModel.GoogleDriveFile, GoogleDriveModel.GoogleDriveFolder,
+    GoogleDriveStructureConfig](GoogleDriveProtocolCreator, config, httpSenderConfig, timeout, spawner)
+
---
+++ src/test/scala/com/github/sync/protocol/gdrive/GoogleDriveProtocolFactorySpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.protocol.gdrive
+
+import akka.actor.typed.ActorSystem
+import akka.util.Timeout
+import com.github.cloudfiles.core.http.factory.{HttpRequestSenderConfig, Spawner}
+import com.github.sync.protocol.config.GoogleDriveStructureConfig
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import scala.concurrent.duration._
+
+class GoogleDriveProtocolFactorySpec extends AnyFlatSpec with Matchers with MockitoSugar {
+  "GoogleDriveProtocolFactory" should "correctly initialize the base class" in {
+    val config = mock[GoogleDriveStructureConfig]
+    val senderConfig = mock[HttpRequestSenderConfig]
+    val timeout = Timeout(100.seconds)
+    val spawner = mock[Spawner]
+    implicit val system: ActorSystem[_] = mock[ActorSystem[_]]
+
+    val factory = new GoogleDriveProtocolFactory(config, senderConfig, timeout, spawner)
+    factory.config should be(config)
+    factory.httpSenderConfig should be(senderConfig)
+    factory.timeout should be(timeout)
+    factory.creator should be(GoogleDriveProtocolCreator)
+  }
+}
