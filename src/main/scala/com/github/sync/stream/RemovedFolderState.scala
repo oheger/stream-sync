0e8bfb171777f7a316667679d5071ec42f7986fc
oheger <oliver.heger@oliver-heger.de>
2021-11-24 22:25:54 +0100

    Extracted logic to track remove operations from MirrorStage.
    Removing folders during a sync process can be tricky. Therefore, this
    logic has been extracted into a dedicated state class. The new class
    is going to be reused by multiple sync stage implementations. It may
    be extended later with additional functionality.

---
+++ src/main/scala/com/github/sync/stream/RemovedFolderState.scala
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.cloudfiles.core.http.UriEncodingHelper
+import com.github.sync.SyncTypes.{FsElement, FsFolder, SyncAction, SyncOperation}
+
+private object RemovedFolderState:
+  /** Constant representing an initial empty state. */
+  final val Empty = RemovedFolderState(Set.empty, List.empty)
+
+  /**
+    * Convenience function to create a ''SyncOperation'' for the given element,
+    * level, and action (typically a remove or a local remove action).
+    *
+    * @param element  the element subject of the operation
+    * @param optLevel the optional level of the operation (defaults to the
+    *                 element's level)
+    * @param action   the action of the operation
+    * @return the newly created ''SyncOperation''
+    */
+  def createRemoveOperation(element: FsElement, optLevel: Option[Int] = None,
+                            action: SyncAction = SyncAction.ActionRemove): SyncOperation =
+    SyncOperation(element, action, optLevel getOrElse element.level, element.id)
+
+  /**
+    * Modifies the URI of the given folder to end on a separator, which is
+    * required for the logic to detect subfolders. Note: As this is an internal
+    * function only, it assumes that all passed in folders do not have a
+    * trailing separator.
+    *
+    * @param folder the folder to update
+    * @return the updated folder
+    */
+  private def updateFolderUri(folder: FsFolder): FsFolder =
+    folder.copy(relativeUri = folder.relativeUri + UriEncodingHelper.UriSeparator)
+
+/**
+  * A helper class to manage folders that have been removed during a sync
+  * process.
+  *
+  * Dealing with removed folders can be tricky for multiple reasons:
+  *  - A folder cannot be safely removed directly, but only after all its
+  *    subelements have been processed. Therefore, the remove operations are
+  *    delayed until the end of the sync process.
+  *  - When processing the content of a removed folder, the information that
+  *    the elements encountered also need to be removed, must be available;
+  *    this needs to be tracked over the whole subtree.
+  *
+  * This supports in implementing these requirements. It stores the roots of
+  * directory trees that need to be removed and offers functionality to check
+  * whether an element is part of such a tree.
+  *
+  * @param roots              the current set of roots that have been removed
+  * @param deferredOperations the operations to be executed at the end of the
+  *                           sync process
+  */
+private case class RemovedFolderState(roots: Set[FsFolder],
+                                      deferredOperations: List[SyncOperation]):
+
+  import RemovedFolderState.*
+
+  /**
+    * Tries to find a root folder that spans a tree, which contains the given
+    * element. If such a folder exists, it is returned in a ''Some'';
+    * otherwise, result is ''None''.
+    *
+    * @param element the element to check
+    * @return an ''Option'' with the removed root folder of this element
+    */
+  def findRoot(element: FsElement): Option[FsFolder] =
+    roots find { root =>
+      element.relativeUri.startsWith(root.relativeUri)
+    }
+
+  /**
+    * Creates a ''SyncOperation'' for the given element and returns an updated
+    * state object. This function must be called for each element that is
+    * removed during the sync process. It contains the logic to defer remove
+    * operations for folders and to keep track of new removed root folders.
+    * The ''optRoot'' parameter can point to a removed root folder, which is a
+    * (direct or indirect) parent of this element; this is normally the result
+    * of a ''findRoot()'' call. Result is a list with the ''SyncOperation''s to
+    * execute immedeately (which can contain at most 1 element, but for stages
+    * doing sync operations a list is typically more convenient than an option)
+    * and the possibly modified state.
+    *
+    * @param element the removed element
+    * @param action  the action for the sync operation (typically a remove or
+    *                a local remove action)
+    * @param optRoot the optional removed parent folder of the element
+    * @return the operations to execute directly and the updated state
+    */
+  def handleRemoveElement(element: FsElement, action: SyncAction, optRoot: Option[FsFolder]):
+  (List[SyncOperation], RemovedFolderState) =
+    val op = createRemoveOperation(element, optRoot map (_.level), action)
+    element match
+      case _: FsFolder if optRoot.isDefined =>
+        (Nil, copy(deferredOperations = op :: deferredOperations))
+      case folder: FsFolder =>
+        val newRoot = updateFolderUri(folder)
+        (Nil, copy(roots = roots + newRoot, deferredOperations = op :: deferredOperations))
+      case _ =>
+        (List(op), this)
+
+  /**
+    * Adds another folder to the set of removed root folders. This function can
+    * be used for folders that are removed by other operations not handled by
+    * ''handleRemoveElement()''; e.g. when a folder is replaced by a file.
+    *
+    * @param folder the folder to add
+    * @return the updated state containing this folder
+    */
+  def addRoot(folder: FsFolder): RemovedFolderState = copy(roots = roots + updateFolderUri(folder))
+  
---
+++ src/test/scala/com/github/sync/stream/RemovedFolderStateSpec.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.{FsFile, FsFolder, SyncAction, SyncOperation}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.time.Instant
+
+class RemovedFolderStateSpec extends AnyFlatSpec, Matchers :
+  "RemoveRoots" should "return None if no root folder is found for an element" in {
+    val element = FsFolder("someID", "/someUri", 11)
+
+    RemovedFolderState.Empty.findRoot(element) should be(None)
+  }
+
+  it should "find the root folder of an element" in {
+    val root = FsFolder("root", "/root", 1)
+    val otherRoot = FsFolder("other", "/another/root", 2)
+    val element = FsFile("file", "/root/path/to/file.txt", 3, Instant.now(), 100)
+    val roots = RemovedFolderState(Set(otherRoot, root), List.empty)
+
+    roots.findRoot(element) should be(Some(root))
+  }
+
+  it should "handle a removed file under a removed root folder" in {
+    val file = FsFile("f1", "/some/path/file.txt", 2, Instant.now(), 1500)
+    val root = FsFolder("root", "/some", 1)
+    val expOp = SyncOperation(file, SyncAction.ActionRemove, root.level, file.id)
+    val state = RemovedFolderState.Empty
+
+    val (ops, nextState) = state.handleRemoveElement(file, SyncAction.ActionRemove, Some(root))
+    nextState should be theSameInstanceAs state
+    ops should contain only expOp
+  }
+
+  it should "handle a removed file not under a removed root folder" in {
+    val file = FsFile("fDel", "/the/removed/file.doc", 2, Instant.now(), 2000)
+    val expOp = SyncOperation(file, SyncAction.ActionLocalRemove, file.level, file.id)
+    val state = RemovedFolderState.Empty
+
+    val (ops, nextState) = state.handleRemoveElement(file, SyncAction.ActionLocalRemove, None)
+    nextState should be theSameInstanceAs state
+    ops should contain only expOp
+  }
+
+  it should "handle a removed folder under a removed root folder" in {
+    val folder = FsFolder("subFolder", "/path/to/delete/deeply/nested/sub", 5)
+    val root = FsFolder("root", "/path/", 1)
+    val rootOp = SyncOperation(root, SyncAction.ActionLocalRemove, root.level, root.id)
+    val expOp = SyncOperation(folder, SyncAction.ActionLocalRemove, root.level, folder.id)
+    val state = RemovedFolderState(Set.empty, List(rootOp))
+
+    val (ops, nextState) = state.handleRemoveElement(folder, SyncAction.ActionLocalRemove, Some(root))
+    ops shouldBe empty
+    nextState.deferredOperations should contain only(expOp, rootOp)
+    nextState.roots shouldBe empty
+  }
+
+  it should "handle a new removed root folder" in {
+    val otherRoot = FsFolder("otherRemovedRoot", "/other/path/to/delete", 3)
+    val otherOp = SyncOperation(otherRoot, SyncAction.ActionRemove, otherRoot.level, otherRoot.id)
+    val FolderPath = "/path/to/delete"
+    val folder = FsFolder("removedRoot", FolderPath, 2)
+    val removedFolder = folder.copy(relativeUri = FolderPath + "/")
+    val expOp = SyncOperation(folder, SyncAction.ActionRemove, folder.level, folder.id)
+    val state = RemovedFolderState(Set(otherRoot), List(otherOp))
+
+    val (ops, nextState) = state.handleRemoveElement(folder, SyncAction.ActionRemove, None)
+    ops shouldBe empty
+    nextState.deferredOperations should contain only(expOp, otherOp)
+    nextState.roots should contain only(removedFolder, otherRoot)
+  }
+
+  it should "allow adding a folder manually" in {
+    val otherRoot = FsFolder("existingRemovedRoot", "/other/path/to/delete", 3)
+    val FolderPath = "/explicit/folder/to/add"
+    val folder = FsFolder("irregularFolder", FolderPath, 3)
+    val addedFolder = folder.copy(relativeUri = FolderPath + "/")
+    val state = RemovedFolderState(Set(otherRoot), List.empty)
+
+    val nextState = state.addRoot(folder)
+    nextState.deferredOperations shouldBe empty
+    nextState.roots should contain only(otherRoot, addedFolder)
+  }
