6c1adb70bf7ad250f9731ccbe6b49da1f4a4fdde
oheger <oliver.heger@oliver-heger.de>
2021-12-03 21:14:02 +0100

    Added an extension to obtain the modified time of an element.
    For sync operations, it is often necessary to query an FsElement for
    its last modified time, no matter whether it is a file or a folder
    (folders do not have such a property). This extension function handles
    this use case in a generic way.

---
+++ src/main/scala/com/github/sync/stream/Extensions.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.{FsElement, FsFile, FsFolder}
+
+import java.time.Instant
+
+extension (elem: FsElement)
+
+/**
+  * Determines the last modified time from an element, no matter whether it is
+  * a file or a folder. When comparing elements during a sync process it is
+  * often required to compare the times of last modification for these
+  * elements. Rather than checking for the concrete element types, this
+  * function allows direct access to the last modification time. If the element
+  * is a file, its ''lastModified'' property is returned; otherwise, result is
+  * the provided folder time.
+  */
+  def modifiedTime(folderTime: => Instant): Instant = elem match
+    case file: FsFile => file.lastModified
+    case _: FsFolder => folderTime
---
+++ src/test/scala/com/github/sync/stream/ExtensionsSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.{FsFile, FsFolder}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.time.Instant
+
+/**
+  * Test class for the extension functions of the stream package.
+  */
+class ExtensionsSpec extends AnyFlatSpec, Matchers :
+  "modifiedTime" should "return the time of a file" in {
+    val FileModifiedTime = Instant.parse("2021-12-03T20:03:37Z")
+    val file = FsFile("fid", "/some/file.txt", 2, FileModifiedTime, 1000)
+
+    val time = file.modifiedTime(throw new UnsupportedOperationException("Unexpected evaluation"))
+    time should be(FileModifiedTime)
+  }
+
+  it should "return the provided time of a folder" in {
+    val FolderModifiedTime = Instant.parse("2021-12-03T20:07:12Z")
+    val folder = FsFolder("folderId", "/some/folder/path", 3)
+
+    val time = folder.modifiedTime(FolderModifiedTime)
+    time should be(FolderModifiedTime)
+  }
