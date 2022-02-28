b63a6981046cc92f020d0bd8e052ac1b2de7743d
oheger <oliver.heger@oliver-heger.de>
2022-02-28 22:22:38 +0100

    Added a Throttle module.
    The current approach to throttling does no longer work because the
    Noop actions emitted for all unchanged elements. They should not be
    subject to throttling, as this slows down a sync stream extremely.
    
    To deal with this issue, a new module has been introduced that
    provides a more sophisticated implementation of throttling that only
    takes "real" actions into account.

---
+++ src/main/scala/com/github/sync/stream/Throttle.scala
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import akka.stream.{Attributes, FanInShape2, FlowShape, Inlet, Outlet}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import com.github.sync.SyncTypes.{FsElement, SyncAction, SyncOperation, SyncOperationResult}
+
+import scala.concurrent.duration.*
+
+/**
+  * A module to support limiting the throughput of operations in the sync
+  * stream.
+  *
+  * While Akka Streams already offers such functionality via the ''throttle''
+  * operator, sync streams have more complex requirements: Not all stream
+  * elements are subject to throttling, but only operations that actually cause
+  * changes on the destination. This especially excludes the Noop actions
+  * generated for elements that do not change.
+  *
+  * This module therefore constructs a sub graph that splits operations and
+  * applies the ''throttle'' operator only to the subset that have a real
+  * effect.
+  */
+object Throttle:
+  /**
+    * Returns a ''Flow'' that applies throttling to the given stage.
+    *
+    * @param stage             the stage subject to throttling
+    * @param operationsPerUnit the number of operations to pass per time unit
+    * @return the decorated ''Flow'' respecting the specified limit
+    */
+  def apply(stage: Flow[SyncOperation, SyncOperationResult, Any], operationsPerUnit: Int):
+  Flow[SyncOperation, SyncOperationResult, Any] =
+    val filterThrottle = Flow[SyncOperation].filter(needsThrottling).throttle(operationsPerUnit, 1.second)
+    val mapToResult = Flow[SyncOperation].map(toResult)
+    Flow.fromGraph(GraphDSL.create() {
+      implicit builder =>
+        import GraphDSL.Implicits.*
+        val broadcast = builder.add(Broadcast[SyncOperation](2))
+        val merge = builder.add(new ThrottleMergeStage)
+        broadcast ~> mapToResult ~> merge.in0
+        broadcast ~> filterThrottle ~> stage ~> merge.in1
+        FlowShape(broadcast.in, merge.out)
+    })
+
+  /**
+    * Generates a ''SyncOperationResult'' for a ''SyncOperation'' that is not
+    * passed to the throttled stage.
+    *
+    * @param op the ''SyncOperation''
+    * @return the result for this operation
+    */
+  private def toResult(op: SyncOperation): SyncOperationResult = SyncOperationResult(op, None)
+
+  /**
+    * Checks whether the given operation is subject to throttling.
+    *
+    * @param op the operation in question
+    * @return a flag whether throttling needs to be applied to this operation
+    */
+  private def needsThrottling(op: SyncOperation): Boolean = op.action != SyncAction.ActionNoop
+
+  /**
+    * A specialized stage implementation that handles the merging of elements
+    * in a throttled sub graph.
+    *
+    * The basic merge stages cannot be used for this purpose, since they do not
+    * retain the order of the original elements. This stage expects that on one
+    * input all stream elements are passed, while on the second input only the
+    * throttled elements come in. If an element that requires throttling is
+    * received over the first input, the second one is pulled, and the element
+    * obtained from there is emitted. Otherwise, the first input channel is
+    * passed downstream.
+    */
+  private class ThrottleMergeStage
+    extends GraphStage[FanInShape2[SyncOperationResult, SyncOperationResult, SyncOperationResult]] :
+    val out: Outlet[SyncOperationResult] = Outlet[SyncOperationResult]("ThrottleMerge.out")
+    val inAll: Inlet[SyncOperationResult] = Inlet[SyncOperationResult]("ThrottleMerge.inAll")
+    val inThrottled: Inlet[SyncOperationResult] = Inlet[SyncOperationResult]("ThrottleMerge.inThrottle")
+
+    override def shape: FanInShape2[SyncOperationResult, SyncOperationResult, SyncOperationResult] =
+      new FanInShape2[SyncOperationResult, SyncOperationResult, SyncOperationResult](inAll, inThrottled, out)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+        /**
+          * The number of input ports that are finished. It is important to
+          * complete this stage only if all ports are finished; otherwise,
+          * elements at the end can be missed.
+          */
+        private var finishedPorts = 0
+
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit =
+            if finishedPorts == 0 then pull(inAll)
+        })
+
+        setHandler(inAll, new InHandler {
+          override def onPush(): Unit =
+            val result = grab(inAll)
+            if needsThrottling(result.op) then pull(inThrottled)
+            else push(out, result)
+
+          override def onUpstreamFinish(): Unit = portFinished()
+        })
+
+        setHandler(inThrottled, new InHandler {
+          override def onPush(): Unit =
+            push(out, grab(inThrottled))
+
+          override def onUpstreamFinish(): Unit = portFinished()
+        })
+
+        /**
+          * Handles a finished input port. If all ports are done, this stage can
+          * be completed.
+          */
+        private def portFinished(): Unit =
+          finishedPorts += 1
+          if finishedPorts == 2 then completeStage()
+      }
---
+++ src/test/scala/com/github/sync/stream/ThrottleSpec.scala
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Flow, Source}
+import akka.testkit.TestKit
+import com.github.sync.AsyncTestHelper
+import com.github.sync.SyncTypes.{FsElement, SyncAction, SyncOperation, SyncOperationResult}
+import com.github.sync.stream.ThrottleSpec.createResult
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.{AnyFlatSpec, AnyFlatSpecLike}
+import org.scalatest.matchers.should.Matchers
+
+import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
+import scala.concurrent.{Await, ExecutionContext, Future, TimeoutException}
+import scala.concurrent.duration.*
+
+object ThrottleSpec:
+  /**
+    * Creates a ''SyncOperation'' for the given parameters.
+    *
+    * @param elem   the affected element
+    * @param action the action
+    * @return the ''SyncOperation''
+    */
+  private def createOp(elem: FsElement, action: SyncAction): SyncOperation =
+    SyncOperation(elem, action, 1, elem.id)
+
+  /**
+    * Creates a result for the given ''SyncOperation''.
+    *
+    * @param op the operation
+    * @return the ''SyncOperationResult'' for the operation
+    */
+  private def createResult(op: SyncOperation): SyncOperationResult = SyncOperationResult(op, None)
+
+  /**
+    * Creates the results for the given sequence of operations.
+    *
+    * @param operations the operations
+    * @return a sequence with the results for these operations
+    */
+  private def createResults(operations: Seq[SyncOperation]): Seq[SyncOperationResult] = operations map createResult
+
+/**
+  * Test class for ''Throttle''.
+  */
+class ThrottleSpec(testSystem: ActorSystem) extends TestKit(testSystem), AnyFlatSpecLike, BeforeAndAfterAll, Matchers,
+  AsyncTestHelper :
+  def this() = this(ActorSystem("ThrottleSpec"))
+
+  override protected def afterAll(): Unit =
+    TestKit shutdownActorSystem system
+    super.afterAll()
+
+  import ThrottleSpec.*
+
+  "Throttle" should "handle an empty stream" in {
+    val helper = new ThrottleTestHelper
+
+    futureResult(helper.runStream(Seq.empty, 100)) shouldBe empty
+  }
+
+  it should "handle a stream consisting only of Noop actions" in {
+    val operations = (1 to 32) map { idx =>
+      createOp(AbstractStageSpec.createFile(idx), SyncAction.ActionNoop)
+    }
+    val helper = new ThrottleTestHelper
+
+    val results = futureResult(helper.runStream(operations, 1))
+    results should contain theSameElementsInOrderAs createResults(operations)
+    helper.expectNoMoreThrottledOperations()
+  }
+
+  it should "handle a stream consisting only of real actions" in {
+    val operations = (1 to 32) map { idx =>
+      createOp(AbstractStageSpec.createFile(idx), SyncAction.ActionRemove)
+    }
+    val expectedResults = createResults(operations)
+    val helper = new ThrottleTestHelper
+
+    val results = futureResult(helper.runStream(operations, 100))
+    results should contain theSameElementsInOrderAs expectedResults
+    helper.nextThrottledOperations(operations.size) should contain theSameElementsInOrderAs operations
+  }
+
+  it should "handle a stream containing operations with different types of actions" in {
+    val operations = List(
+      createOp(AbstractStageSpec.createFile(1), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(2), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(3), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(4), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(5), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(6), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(7), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(8), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(9), SyncAction.ActionNoop)
+    )
+    val expectedResults = createResults(operations)
+    val helper = new ThrottleTestHelper
+
+    val results = futureResult(helper.runStream(operations, 100))
+    results should contain theSameElementsInOrderAs expectedResults
+  }
+
+  it should "apply the throttling to the passed in stage" in {
+    val operations = (1 to 4) map { idx =>
+      createOp(AbstractStageSpec.createFile(idx), SyncAction.ActionRemove)
+    }
+    val expectedResults = createResults(operations)
+    val helper = new ThrottleTestHelper
+
+    val futResult = helper.runStream(operations, 1)
+    intercept[TimeoutException] {
+      Await.ready(futResult, 2.seconds)
+    }
+  }
+
+  it should "keep the order even if operations are throttled" in {
+    val operations = List(
+      createOp(AbstractStageSpec.createFile(1), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(2), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(3), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(4), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(5), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(6), SyncAction.ActionNoop),
+      createOp(AbstractStageSpec.createFile(7), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(8), SyncAction.ActionRemove),
+      createOp(AbstractStageSpec.createFile(9), SyncAction.ActionNoop)
+    )
+    val expectedResults = createResults(operations)
+    val helper = new ThrottleTestHelper
+
+    val results = futureResult(helper.runStream(operations, 2))
+    results should contain theSameElementsInOrderAs expectedResults
+  }
+
+  private class ThrottleTestHelper:
+    /**
+      * A queue where to store the operations passed through the throttled
+      * stage.
+      */
+    private val throttledOperations = new LinkedBlockingQueue[SyncOperation]()
+
+    /**
+      * Executes a stream that passes the given operations through a throttled
+      * test stage.
+      *
+      * @param operations the operations to pass through the stream
+      * @param opsPerUnit the number of allowed operations per time unit
+      * @return A ''Future'' with the output of the throttled stage
+      */
+    def runStream(operations: Seq[SyncOperation], opsPerUnit: Int): Future[Seq[SyncOperationResult]] =
+      implicit val ec: ExecutionContext = system.dispatcher
+      val source = Source(operations)
+      val stage = Flow[SyncOperation].map { op =>
+        throttledOperations.offer(op)
+        createResult(op)
+      }
+      val throttledStage = Throttle(stage, opsPerUnit)
+      val sink = AbstractStageSpec.foldSink[SyncOperationResult]
+      source.via(throttledStage).runWith(sink).map(_.reverse)
+
+    /**
+      * Returns the next operation that has been passed to the throttled stage.
+      * If there is no such operation, the test fails.
+      *
+      * @return the next throttled operation
+      */
+    def nextThrottledOperation: SyncOperation =
+      val op = throttledOperations.poll(3, TimeUnit.SECONDS)
+      op should not be null
+      op
+
+    /**
+      * Returns the given number of operations passed to the throttled stage.
+      * If there is a timeout when querying these operations, the test fails.
+      *
+      * @param count the number of operations to query
+      * @return a sequence with the operations that have been obtained
+      */
+    def nextThrottledOperations(count: Int): Seq[SyncOperation] =
+      (1 to count) map (_ => nextThrottledOperation)
+
+    /**
+      * Checks that no more operations have been passed to the throttled stage.
+      */
+    def expectNoMoreThrottledOperations(): Unit =
+      val op = throttledOperations.poll(500, TimeUnit.MILLISECONDS)
+      op should be(null)
