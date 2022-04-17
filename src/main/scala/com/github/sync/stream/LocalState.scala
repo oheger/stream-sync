f8a0289cadb75894bd3a31a5fb5e8700b15e244c
oheger <oliver.heger@oliver-heger.de>
2022-04-17 21:51:40 +0200

    Added LocalState module.
    The module contains functionality related to the local state of sync
    processes. So far, a helper function has been implemented that tests
    whether a given SyncOperation has an impact on the local state.

---
+++ src/main/scala/com/github/sync/stream/LocalState.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.{FsElement, SyncAction, SyncOperation}
+
+/**
+  * A module providing functionality related to the local state of sync
+  * processes.
+  *
+  * Bidirectional sync processes require information of the local elements from
+  * the last process. That way, changes on local elements can be detected,
+  * which is a precondition for detecting many types of conflicts. A file with
+  * the local state is one input of the sync process; depending on the
+  * operations performed, the state is updated during the process.
+  *
+  * This module offers some type definitions and helper functions for dealing
+  * with local state. This includes serialization facilities, but also
+  * transformations to apply sync operations on local elements.
+  */
+private object LocalState:
+  /**
+    * A data class to represent an element in the local state of a sync
+    * process.
+    *
+    * The representation consists of the element with some additional metadata.
+    * Elements that have been removed are still stored in the local state, but
+    * marked with a flag. This is necessary to resume interrupted sync
+    * processes. (If removed elements were missing in the local state, it would
+    * not be clear whether they have been processed in the interrupted process
+    * or not.)
+    *
+    * @param element the actual element
+    * @param removed flag whether this element has been removed
+    */
+  case class LocalElementState(element: FsElement,
+                               removed: Boolean)
+
+  /** A set with the action types that have an affect of the local state. */
+  private val ActionsAffectingLocalState = Set(SyncAction.ActionLocalCreate, SyncAction.ActionLocalOverride,
+    SyncAction.ActionLocalRemove)
+
+  extension (operation: SyncOperation)
+
+  /**
+    * Returns a flag whether this ''SyncOperation'' affects the local
+    * element state.
+    */
+    def affectsLocalState: Boolean = ActionsAffectingLocalState(operation.action)
---
+++ src/test/scala/com/github/sync/stream/LocalStateSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.{FsFolder, SyncAction, SyncOperation}
+import com.github.sync.stream.LocalState.affectsLocalState
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+/**
+  * Test class for ''LocalState''.
+  */
+class LocalStateSpec extends AnyFlatSpec, Matchers:
+  /**
+    * Helper function to check the ''affectsLocalState'' extension function.
+    *
+    * @param action         the action of the test operation
+    * @param expectedResult the expected result
+    */
+  private def checkAffectsLocalState(action: SyncAction, expectedResult: Boolean): Unit =
+    val op = SyncOperation(FsFolder("f1", "/a/folder", 2), action, 2, "f2")
+    op.affectsLocalState shouldBe expectedResult
+
+  "LocalState" should "detect that a SyncOperation with ActionNoop does not affect the local state" in {
+    checkAffectsLocalState(SyncAction.ActionNoop, expectedResult = false)
+  }
+
+  it should "detect that a SyncOperation with ActionRemove does not affect the local state" in {
+    checkAffectsLocalState(SyncAction.ActionRemove, expectedResult = false)
+  }
+
+  it should "detect that a SyncOperation with ActionCreate does not affect the local state" in {
+    checkAffectsLocalState(SyncAction.ActionCreate, expectedResult = false)
+  }
+
+  it should "detect that a SyncOperation with ActionOverride does not affect the local state" in {
+    checkAffectsLocalState(SyncAction.ActionOverride, expectedResult = false)
+  }
+
+  it should "detect that a SyncOperation with ActionLocalRemove affects the local state" in {
+    checkAffectsLocalState(SyncAction.ActionLocalRemove, expectedResult = true)
+  }
+
+  it should "detect that a SyncOperation with ActionLocalCreate affects the local state" in {
+    checkAffectsLocalState(SyncAction.ActionLocalCreate, expectedResult = true)
+  }
+
+  it should "detect that a SyncOperation with ActionLocalOverride affects the local state" in {
+    checkAffectsLocalState(SyncAction.ActionLocalOverride, expectedResult = true)
+  }
