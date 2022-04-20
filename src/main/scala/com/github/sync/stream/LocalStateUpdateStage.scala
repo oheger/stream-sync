132555d8dd8d9377719e7a76cb769fdfdae05c80
oheger <oliver.heger@oliver-heger.de>
2022-04-20 20:41:42 +0200

    Added LocalStateUpdateStage class.
    This is a special stage implementation that processes the results from
    a SyncStage and the results of executed sync operations. Based on this
    information, it generates the local state needed for the next sync
    process.
    
    The current implementation is not yet complete; some corner cases
    need to be handled in upcoming commits.

---
+++ src/main/scala/com/github/sync/stream/LocalStateUpdateStage.scala
@@ -0,0 +1,265 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import akka.stream.{Attributes, FanInShape2, Inlet, Outlet}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import com.github.sync.SyncTypes.{FsElement, SyncAction, SyncConflictException, SyncOperation, SyncOperationResult}
+import com.github.sync.stream.LocalState.affectsLocalState
+import com.github.sync.stream.LocalStateStage.ElementWithDelta
+import com.github.sync.stream.LocalStateUpdateStage.{LocalAffectingUpdate, NonLocalAffectingUpdate, StateUpdate, filterNones, hasNonDeferredOperations}
+
+private object LocalStateUpdateStage:
+  /**
+    * Checks whether the given sequence contains at least one non-deferred
+    * ''SyncOperation''.
+    *
+    * @param operations the sequence with operations to check
+    * @return '''true''' if there is at least one non-deferred operation;
+    *         '''false''' otherwise
+    */
+  private def hasNonDeferredOperations(operations: Seq[SyncOperation]): Boolean = operations.exists(!_.deferred)
+
+  /**
+    * Checks whether the given conflict contains at least one non-deferred
+    * ''SyncOperation''.
+    *
+    * @param conflict the ''SyncConflictException'' to check
+    * @return '''true''' if there is at least one non-deferred operation;
+    *         '''false''' otherwise
+    */
+  private def hasNonDeferredOperations(conflict: SyncConflictException): Boolean =
+    hasNonDeferredOperations(conflict.localOperations) || hasNonDeferredOperations(conflict.remoteOperations)
+
+  /**
+    * Convenience function to filter a list with ''Option'' elements to keep
+    * only the values of the defined ones.
+    *
+    * @param list the list to process
+    * @tparam T the type of the element values
+    * @return the list containing only the defined values
+    */
+  private def filterNones[T](list: Iterable[Option[T]]): Iterable[T] =
+    list.filter(_.isDefined).map(_.get)
+
+  /**
+    * Converts an optional ''ElementWithDelta'' representing an element from
+    * the original local state to an element of the updated state.
+    *
+    * @param optLocalElement the optional original element
+    * @return the optional element of the next state
+    */
+  private def toLocalState(optLocalElement: Option[ElementWithDelta]): Option[LocalState.LocalElementState] =
+    optLocalElement map (elem => LocalState.LocalElementState(elem.element, removed = false))
+
+  /**
+    * A trait representing an update of the local state for a single element.
+    * Instances of this trait are stored in [[LocalStateUpdateStage]] for the
+    * elements that are currently processed. The purpose of this trait is
+    * finding out when all required information is available and then obtain
+    * the data to write the next local state. Concrete implementations handle
+    * different kinds of state updates.
+    */
+  private sealed trait StateUpdate:
+    /**
+      * Checks whether all information for this update is available. The passed
+      * in function can be used to query whether a result for a specific
+      * operation is already available.
+      *
+      * @param opFunc the function to check for an operation result
+      * @return a flag whether this update is now complete
+      */
+    def canUpdate(opFunc: SyncOperation => Option[SyncOperationResult]): Boolean
+
+    /**
+      * Returns the data to update the local state for the associated element.
+      * This function is called if ''canUpdate()'' has returned '''true'''.
+      * The passed in function allows querying the result for a specific
+      * operation, which may be required to generate the update data. The
+      * return value if a tuple with the element to emit downstream and an
+      * optional operation this update refers to; this operation is then
+      * considered complete and is removed from the set of current operations
+      * maintained by [[LocalStateUpdateStage]].
+      *
+      * @param opFunc the function to query the result for an operation
+      * @return data to process this update
+      */
+    def updateData(opFunc: SyncOperation => SyncOperationResult):
+    (Option[LocalState.LocalElementState], Option[SyncOperation])
+
+  /**
+    * A specialized [[StateUpdate]] caused by an operation that affects a local
+    * element.
+    *
+    * @param optLocalElement the original local element if available
+    * @param operation       the associated operation
+    */
+  private case class LocalAffectingUpdate(optLocalElement: Option[ElementWithDelta],
+                                          operation: SyncOperation) extends StateUpdate :
+    override def canUpdate(opFunc: SyncOperation => Option[SyncOperationResult]): Boolean =
+      opFunc(operation).isDefined
+
+    override def updateData(opFunc: SyncOperation => SyncOperationResult):
+    (Option[LocalState.LocalElementState], Option[SyncOperation]) =
+      val result = opFunc(operation)
+      val element = if result.optFailure.isEmpty then
+        Some(LocalState.LocalElementState(result.op.element,
+          removed = result.op.action == SyncAction.ActionLocalRemove))
+      else
+        toLocalState(optLocalElement)
+      (element, Some(operation))
+
+  /**
+    * A specialized [[StateUpdate]] caused by an operation that does not affect
+    * the local element it is associated with. Hence, the element can be output
+    * as soon as all previous operations are complete.
+    *
+    * @param optLocalElement the local element if available
+    */
+  private case class NonLocalAffectingUpdate(optLocalElement: Option[ElementWithDelta]) extends StateUpdate :
+    override def canUpdate(opFunc: SyncOperation => Option[SyncOperationResult]): Boolean = true
+
+    override def updateData(opFunc: SyncOperation => SyncOperationResult):
+    (Option[LocalState.LocalElementState], Option[SyncOperation]) =
+      (toLocalState(optLocalElement), None)
+end LocalStateUpdateStage
+
+/**
+  * A ''Stage'' that updates the local state of a sync process.
+  *
+  * This stage receives input from a [[SyncStage]] and from a stage that
+  * applies sync operations. Based on this information, it produces a stream of
+  * elements that represent the updated state of a sync process - i.e. a
+  * snapshot of the elements available locally after the process has finished.
+  *
+  * The applied sync operations need to be taken into account, since the new
+  * local state depends on their success: depending on the type of sync
+  * actions, local elements have been manipulated or not.
+  *
+  * It is important that the new local elements are written in the correct
+  * order. However, the incoming sync operations may not necessarily be in this
+  * order.
+  */
+private class LocalStateUpdateStage
+  extends GraphStage[FanInShape2[SyncStage.SyncStageResult, SyncOperationResult, LocalState.LocalElementState]] :
+  val out: Outlet[LocalState.LocalElementState] = Outlet[LocalState.LocalElementState]("LocalStateUpdateStage.out")
+  val inSync: Inlet[SyncStage.SyncStageResult] = Inlet[SyncStage.SyncStageResult]("LocalStateUpdateStage.inSync")
+  val inOps: Inlet[SyncOperationResult] = Inlet[SyncOperationResult]("LocalStateUpdateStage.inOps")
+
+  override def shape: FanInShape2[SyncStage.SyncStageResult, SyncOperationResult, LocalState.LocalElementState] =
+    new FanInShape2[SyncStage.SyncStageResult, SyncOperationResult, LocalState.LocalElementState](inSync, inOps, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+    /** Stores the results of operations that are currently processed. */
+    private var operationResults = Map.empty[SyncOperation, SyncOperationResult]
+
+    /** Stores the updates that are currently processed. */
+    private var currentUpdates = Vector.empty[StateUpdate]
+
+    /** Flag whether the source with sync results is complete. */
+    private var syncResultsComplete = false
+
+    /** Flag whether the source with sync operations is complete. */
+    private var operationsComplete = false
+
+    setHandler(inSync, new InHandler {
+      override def onPush(): Unit =
+        handleSyncResult(grab(inSync))
+        pull(inSync)
+
+      override def onUpstreamFinish(): Unit =
+        syncResultsComplete = true
+        sourceCompleted()
+    })
+
+    setHandler(inOps, new InHandler {
+      override def onPush(): Unit =
+        val opResult = grab(inOps)
+        operationResults += opResult.op -> opResult
+        emitCompletedOperations()
+        pull(inOps)
+
+      override def onUpstreamFinish(): Unit =
+        operationsComplete = true
+        sourceCompleted()
+    })
+
+    setHandler(out, new OutHandler {
+      override def onPull(): Unit = {}
+    })
+
+    override def preStart(): Unit =
+      pull(inSync)
+      pull(inOps)
+
+    /**
+      * Handles a sync result. If this result requires an update, it is
+      * processed in the correct order.
+      *
+      * @param syncResult the current sync result
+      */
+    private def handleSyncResult(syncResult: SyncStage.SyncStageResult): Unit =
+      updateForResult(syncResult) foreach { update =>
+        currentUpdates = currentUpdates :+ update
+        emitCompletedOperations()
+      }
+
+    /**
+      * Returns a [[StateUpdate]] object for the given sync result. If this
+      * result does not require any update, ''None'' is returned.
+      *
+      * @param syncResult the current sync result
+      * @return an ''Option'' with the update for this result
+      */
+    private def updateForResult(syncResult: SyncStage.SyncStageResult): Option[StateUpdate] =
+      syncResult.elementResult match
+        case Right(operations) =>
+          operations filterNot (_.deferred) match
+            case h :: _ =>
+              if h.affectsLocalState then
+                Some(LocalAffectingUpdate(syncResult.optLocalElement, h))
+              else
+                Some(NonLocalAffectingUpdate(syncResult.optLocalElement))
+            case _ => None
+
+        case Left(conflict) if hasNonDeferredOperations(conflict) =>
+          Some(NonLocalAffectingUpdate(syncResult.optLocalElement))
+
+        case _ => None
+
+    /**
+      * Checks whether all input sources have been completed. If so, completes
+      * the whole stage.
+      */
+    private def sourceCompleted(): Unit =
+      if syncResultsComplete && operationsComplete then
+        completeStage()
+
+    /**
+      * Generates the output for the sync operations that have already been
+      * completed.
+      */
+    private def emitCompletedOperations(): Unit =
+      val (complete, incomplete) = currentUpdates span (_.canUpdate(operationResults.get))
+      val (emitData, operations) = complete.map { update =>
+        update.updateData(operationResults.apply)
+      }.unzip
+
+      emitMultiple(out, filterNones(emitData).toVector)
+      currentUpdates = incomplete
+      operationResults --= filterNones(operations)
+  }
---
+++ src/test/scala/com/github/sync/stream/LocalStateUpdateStageSpec.scala
@@ -0,0 +1,288 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+import akka.testkit.TestKit
+import com.github.sync.SyncTypes.{FsElement, FsFile, SyncAction, SyncConflictException, SyncElementResult, SyncOperation, SyncOperationResult}
+import com.github.sync.stream.LocalStateStage.ElementWithDelta
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import java.io.IOException
+import java.time.Instant
+import scala.concurrent.duration.*
+
+object LocalStateUpdateStageSpec:
+  /** A test file used by test cases. */
+  private val TestElement = AbstractStageSpec.createFile(111)
+
+  /**
+    * Generates a test sync operation based on the given properties.
+    *
+    * @param action   the action
+    * @param element  the element affected
+    * @param deferred the deferred flag
+    * @return the test sync operation
+    */
+  private def testOperation(action: SyncAction, element: FsElement = TestElement, deferred: Boolean = false):
+  SyncOperation =
+    SyncOperation(element, action, element.level, element.id, deferred)
+
+  /**
+    * Generates a successful sync result that contains only the given
+    * operation.
+    *
+    * @param op the operation
+    * @return the result containing this operation
+    */
+  private def syncResult(op: SyncOperation): SyncElementResult = Right(List(op))
+
+  /**
+    * Generates a test ''ElementWithDelta'' based on the given properties.
+    *
+    * @param element    the referenced element
+    * @param changeType the change type
+    * @return the resulting ''ElementWithDelta''
+    */
+  private def testElementWithDelta(element: FsElement = TestElement,
+                                   changeType: LocalStateStage.ChangeType = LocalStateStage.ChangeType.Changed):
+  ElementWithDelta =
+    ElementWithDelta(element, changeType, Instant.EPOCH)
+
+  /**
+    * Generates a ''Source'' for the given elements that issues its items with
+    * a slight delay. This is used to test that input at the ports is processed
+    * correctly, even if it arrives in unexpected order.
+    *
+    * @param items the items to be emitted by the source
+    * @tparam T the type of the items
+    * @return the source emitting these items with a delay
+    */
+  private def delayedSource[T](items: List[T]): Source[T, NotUsed] =
+    Source(items).delay(50.millis)
+
+/**
+  * Test class for ''LocalStateUpdateStage''.
+  */
+class LocalStateUpdateStageSpec(testSystem: ActorSystem) extends AbstractStageSpec(testSystem) :
+  def this() = this(ActorSystem("LocalStateUpdateStageSpec"))
+
+  import LocalStateUpdateStageSpec.*
+
+  "LocalStateUpdateStage" should "directly output elements not affected by operations" in {
+    val localElement = testElementWithDelta()
+    val result = SyncStage.SyncStageResult(syncResult(testOperation(SyncAction.ActionCreate)), Some(localElement))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), Nil)
+    nextState should contain only LocalState.LocalElementState(TestElement, removed = false)
+  }
+
+  it should "handle an unavailable local element in a sync result" in {
+    val result = SyncStage.SyncStageResult(syncResult(testOperation(SyncAction.ActionCreate)), None)
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), Nil)
+    nextState shouldBe empty
+  }
+
+  it should "not output data for a result containing only deferred operations" in {
+    val operations = (1 to 4).map { idx =>
+      val element = AbstractStageSpec.createFile(idx)
+      testOperation(SyncAction.ActionLocalOverride, element = element, deferred = true)
+    }
+    val result = SyncStage.SyncStageResult(Right(operations), Some(testElementWithDelta()))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), Nil)
+    nextState shouldBe empty
+  }
+
+  it should "output the original element in case of a conflict result with non-deferred operations" in {
+    val conflict = SyncConflictException(localOperations = List(testOperation(SyncAction.ActionLocalOverride)),
+      remoteOperations = List(testOperation(SyncAction.ActionOverride, AbstractStageSpec.createFolder(2))))
+    val result = SyncStage.SyncStageResult(Left(conflict), Some(testElementWithDelta()))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), Nil)
+    nextState should contain only LocalState.LocalElementState(TestElement, removed = false)
+  }
+
+  it should "handle an unavailable local element in a conflict result" in {
+    val conflict = SyncConflictException(localOperations = List(testOperation(SyncAction.ActionLocalOverride)),
+      remoteOperations = List(testOperation(SyncAction.ActionOverride, AbstractStageSpec.createFolder(2))))
+    val result = SyncStage.SyncStageResult(Left(conflict), None)
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), Nil)
+    nextState shouldBe empty
+  }
+
+  it should "output a modified local element in case of an override operation" in {
+    val localElement = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val syncSource = Source(List(SyncStage.SyncStageResult(syncResult(operation),
+      Some(testElementWithDelta(localElement)))))
+    val opSource = delayedSource(List(opResult))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, syncSource, opSource)
+    nextState should contain only LocalState.LocalElementState(remoteElement, removed = false)
+  }
+
+  it should "output a modified local element in case of an override operation if elements come in different order" in {
+    val localElement = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val syncSource = delayedSource(List(SyncStage.SyncStageResult(syncResult(operation),
+      Some(testElementWithDelta(localElement)))))
+    val opSource = Source(List(opResult))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, syncSource, opSource)
+    nextState should contain only LocalState.LocalElementState(remoteElement, removed = false)
+  }
+
+  it should "keep the order of sync results if operation results arrive later" in {
+    val localElement1 = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val result1 = SyncStage.SyncStageResult(syncResult(operation), Some(testElementWithDelta(localElement1)))
+    val localElement2 = testElementWithDelta()
+    val result2 = SyncStage.SyncStageResult(syncResult(testOperation(SyncAction.ActionCreate)), Some(localElement2))
+    val syncSource = Source(List(result1, result2))
+    val opSource = delayedSource(List(opResult))
+    val expState = List(LocalState.LocalElementState(remoteElement, removed = false),
+      LocalState.LocalElementState(TestElement, removed = false))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, syncSource, opSource)
+    nextState should contain theSameElementsInOrderAs expState
+  }
+
+  it should "keep the order of conflict results if operation results arrive later" in {
+    val localElement1 = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val result1 = SyncStage.SyncStageResult(syncResult(operation), Some(testElementWithDelta(localElement1)))
+    val conflict = SyncConflictException(localOperations = List(testOperation(SyncAction.ActionLocalOverride)),
+      remoteOperations = List(testOperation(SyncAction.ActionOverride, AbstractStageSpec.createFolder(2))))
+    val result2 = SyncStage.SyncStageResult(Left(conflict), Some(testElementWithDelta()))
+    val syncSource = Source(List(result1, result2))
+    val opSource = delayedSource(List(opResult))
+    val expState = List(LocalState.LocalElementState(remoteElement, removed = false),
+      LocalState.LocalElementState(TestElement, removed = false))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, syncSource, opSource)
+    nextState should contain theSameElementsInOrderAs expState
+  }
+
+  it should "keep the order of results if operation results arrive in different order" in {
+    val localElement1 = AbstractStageSpec.createFile(1)
+    val remoteElement1 = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation1 = testOperation(SyncAction.ActionLocalOverride, remoteElement1)
+    val opResult1 = SyncOperationResult(operation1, optFailure = None)
+    val result1 = SyncStage.SyncStageResult(syncResult(operation1), Some(testElementWithDelta(localElement1)))
+    val localElement2 = AbstractStageSpec.createFile(2)
+    val remoteElement2 = AbstractStageSpec.createFile(2, idType = "remote", deltaTime = 20)
+    val operation2 = testOperation(SyncAction.ActionLocalOverride, remoteElement2)
+    val opResult2 = SyncOperationResult(operation2, optFailure = None)
+    val result2 = SyncStage.SyncStageResult(syncResult(operation2), Some(testElementWithDelta(localElement2)))
+    val expState = List(LocalState.LocalElementState(remoteElement1, removed = false),
+      LocalState.LocalElementState(remoteElement2, removed = false))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result1, result2), List(opResult2, opResult1))
+    nextState should contain theSameElementsInOrderAs expState
+  }
+
+  it should "remove completed updates from the internal state" in {
+    val localElement1 = AbstractStageSpec.createFile(1)
+    val remoteElement1 = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation1 = testOperation(SyncAction.ActionLocalOverride, remoteElement1)
+    val opResult1 = SyncOperationResult(operation1, optFailure = None)
+    val result1 = SyncStage.SyncStageResult(syncResult(operation1), Some(testElementWithDelta(localElement1)))
+    val localElement2 = AbstractStageSpec.createFile(2)
+    val remoteElement2 = AbstractStageSpec.createFile(2, idType = "remote", deltaTime = 20)
+    val operation2 = testOperation(SyncAction.ActionLocalOverride, remoteElement2)
+    val opResult2 = SyncOperationResult(operation2, optFailure = None)
+    val result2 = SyncStage.SyncStageResult(syncResult(operation2), Some(testElementWithDelta(localElement2)))
+    val expState = List(LocalState.LocalElementState(remoteElement1, removed = false),
+      LocalState.LocalElementState(remoteElement2, removed = false))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result1, result2), List(opResult1, opResult2))
+    nextState should contain theSameElementsInOrderAs expState
+  }
+
+  it should "remove processed operation results from the internal state" in {
+    val localElement = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val result = SyncStage.SyncStageResult(syncResult(operation), Some(testElementWithDelta(localElement)))
+    val syncSource = delayedSource(List(result, result))
+    val opSource = Source(List(opResult))
+    val expState = List(LocalState.LocalElementState(remoteElement, removed = false))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, syncSource, opSource)
+    nextState should contain theSameElementsAs expState
+  }
+
+  it should "correctly handle a failed sync operation" in {
+    val localElement = AbstractStageSpec.createFile(1)
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalOverride, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = Some(new IOException("Failed")))
+    val result = SyncStage.SyncStageResult(syncResult(operation), Some(testElementWithDelta(localElement)))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), List(opResult))
+    nextState should contain only LocalState.LocalElementState(localElement, removed = false)
+  }
+
+  it should "correctly handle a failed sync operation without a local element" in {
+    val remoteElement = AbstractStageSpec.createFile(1, idType = "remote", deltaTime = 10)
+    val operation = testOperation(SyncAction.ActionLocalCreate, remoteElement)
+    val opResult = SyncOperationResult(operation, optFailure = Some(new IOException("Failed")))
+    val result = SyncStage.SyncStageResult(syncResult(operation), None)
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), List(opResult))
+    nextState shouldBe empty
+  }
+
+  it should "handle an operation to remove a local element" in {
+    val localElement = AbstractStageSpec.createFolder(1)
+    val operation = testOperation(SyncAction.ActionLocalRemove, localElement)
+    val opResult = SyncOperationResult(operation, optFailure = None)
+    val result = SyncStage.SyncStageResult(syncResult(operation), Some(testElementWithDelta(localElement)))
+    val stage = new LocalStateUpdateStage
+
+    val nextState = runStage(stage, List(result), List(opResult))
+    nextState should contain only LocalState.LocalElementState(localElement, removed = true)
+  }
