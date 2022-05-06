9f363057b4fcc76b2a31f217ae8619556150ee86
oheger <oliver.heger@oliver-heger.de>
2022-05-06 21:20:36 +0200

    Added IgnoreTimeDelta class.
    The class represents the delta in the timestamps between to files that
    is ignored before these elements are considered different. It also
    contains the comparison logic.
    
    The class is going to replace the delta value and comparison
    functionality in MirrorSink. Since similar logic needs to be added to
    SyncStage, it makes sense to refactor the logic into a separate class.
    Also, having a dedicated value type for the ignore-time-delta property
    makes the code more readable.
    
    Signed-off-by: oheger <oliver.heger@oliver-heger.de>

---
+++ src/main/scala/com/github/sync/stream/IgnoreTimeDelta.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import com.github.sync.SyncTypes.FsFile
+import com.github.sync.stream.IgnoreTimeDelta.extractSeconds
+
+import java.time.Instant
+import scala.concurrent.duration.FiniteDuration
+
+object IgnoreTimeDelta:
+  /**
+    * Constant for an instance that forces an exact comparison of file
+    * timestamps. The delta to be ignored is set to 0.
+    */
+  final val Zero: IgnoreTimeDelta = new IgnoreTimeDelta(0)
+
+  /**
+    * Creates a new ''IgnoreTimeDelta'' class with the provided duration as
+    * delta. Note that only seconds are accepted as delta, so the passed in
+    * value is rounded downwards to seconds.
+    *
+    * @param delta the duration to accept as a delta
+    * @return the new ''IgnoreTimeDelta'' instance with this delta
+    */
+  def apply(delta: FiniteDuration): IgnoreTimeDelta =
+    require(delta.toSeconds >= 0, "Delta to ignore must be >= 0, but was " + delta)
+    new IgnoreTimeDelta(delta.toSeconds.toInt)
+
+  /**
+    * Extracts the seconds to be compared from the given ''Instant''.
+    *
+    * @param time the time to process
+    * @return the seconds of this time to be compared
+    */
+  private def extractSeconds(time: Instant): Long = time.getEpochSecond
+
+/**
+  * A data class representing a time delta that is to be ignored when comparing
+  * the timestamps of two elements.
+  *
+  * Not all file systems store modification times with the same granularity. So
+  * when comparing elements from different file systems, it could be the case
+  * that the elements are considered different because of a small delta in
+  * their timestamps due to the different resolution used by these file
+  * systems. To fix this, small deltas in timestamps - typically in the range
+  * of 1 to 2 seconds - can be ignored by sync processes. This class represents
+  * the concrete delta to be ignored.
+  *
+  * @param deltaSec the delta in seconds to be ignored
+  */
+class IgnoreTimeDelta private(val deltaSec: Int) extends AnyVal:
+  /**
+    * Compares the passed in timestamps and returns a flag whether they are
+    * different, taking the allowed delta into account.
+    *
+    * @param time1 timestamp 1
+    * @param time2 timestamp 2
+    * @return a flag whether these timestamps are considered different
+    */
+  def isDifferentTimes(time1: Instant, time2: Instant): Boolean =
+    math.abs(extractSeconds(time1) - extractSeconds(time2)) > deltaSec
+
+  /**
+    * Compares the timestamps of the passed in files and returns a flag whether
+    * they are different, taking the allowed delta into account.
+    *
+    * @param file1 file 1
+    * @param file2 file 2
+    * @return a flag whether these files have timestamps considered as
+    *         different
+    */
+  def isDifferentFileTimes(file1: FsFile, file2: FsFile): Boolean =
+    isDifferentTimes(file1.lastModified, file2.lastModified)
---
+++ src/test/scala/com/github/sync/stream/IgnoreTimeDeltaSpec.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.stream
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration.*
+
+/**
+  * Test class for ''IgnoreTimeDelta''.
+  */
+class IgnoreTimeDeltaSpec extends AnyFlatSpec, Matchers :
+  "IgnoreTimeDelta" should "define a constant to ignore no deltas" in {
+    IgnoreTimeDelta.Zero.deltaSec should be(0)
+  }
+
+  it should "prevent the creation of instances with a negative delta" in {
+    intercept[IllegalArgumentException] {
+      IgnoreTimeDelta(-1.second)
+    }
+  }
+
+  it should "allow creating an instance with a delta of 0" in {
+    val ignoreDelta = IgnoreTimeDelta(0.seconds)
+
+    ignoreDelta.deltaSec should be(0)
+  }
+
+  it should "compare two files with a delta greater than the configured threshold" in {
+    val ignoreDelta = IgnoreTimeDelta(5.seconds)
+    val file1 = AbstractStageSpec.createFile(1)
+    val file2 = AbstractStageSpec.createFile(1, deltaTime = ignoreDelta.deltaSec + 1)
+
+    ignoreDelta.isDifferentFileTimes(file1, file2) shouldBe true
+  }
+
+  it should "compare two files with a delta equal to the configured threshold" in {
+    val ignoreDelta = IgnoreTimeDelta(11.seconds)
+    val file1 = AbstractStageSpec.createFile(1)
+    val file2 = AbstractStageSpec.createFile(1, deltaTime = ignoreDelta.deltaSec)
+
+    ignoreDelta.isDifferentFileTimes(file1, file2) shouldBe false
+  }
+
+  it should "use the absolute time delta when comparing to elements" in {
+    val ignoreDelta = IgnoreTimeDelta(5.seconds)
+    val file1 = AbstractStageSpec.createFile(1, deltaTime = ignoreDelta.deltaSec + 1)
+    val file2 = AbstractStageSpec.createFile(1)
+
+    ignoreDelta.isDifferentFileTimes(file1, file2) shouldBe true
+  }
+
+  it should "compare two files with identical timestamps" in {
+    val file1 = AbstractStageSpec.createFile(1)
+    val file2 = AbstractStageSpec.createFile(1)
+
+    IgnoreTimeDelta.Zero.isDifferentFileTimes(file1, file2) shouldBe false
+  }
