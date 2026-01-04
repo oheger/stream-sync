7cc3b2c2281f54969a288249f60117478744a012
Oliver Heger <oliver.heger@oliver-heger.de>
2026-01-04 22:00:08 +0100

    cli: Added initial `CredentialsParameterManager` object.
    This object is responsible for defining the command line options of
    another command line application that allows managing a credentials
    file. Such a file stores passwords in an encrypted way that can be
    referenced from Sync operations.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ src/test/scala/com/github/sync/cli/credentials/CredentialsParameterManagerSpec.scala
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018-2026 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.sync.cli.credentials
+
+import com.github.scli.ParameterExtractor.{ExtractionContext, ParameterExtractionException, Parameters, tryExtractor}
+import com.github.scli.{ConsoleReader, DummyConsoleReader, ParameterParser}
+import com.github.sync.cli.{CliActorSystemLifeCycle, ExtractorTestHelper}
+import org.mockito.Mockito.when
+import org.scalatest.Assertion
+import org.scalatest.Inspectors.forEvery
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import scala.concurrent.Future
+
+object CredentialsParameterManagerSpec:
+  /** The test path to the credentials file. */
+  private val CredentialsFilePath = "/my/credentials/file.crypt"
+
+  /** The test secret to encrypt the credentials file. */
+  private val TestSecret = "my-credentials-secret"
+
+  /**
+    * Creates a map with default values for the basic command line options.
+    *
+    * @param command the name of the command
+    * @return the map with default option values
+    */
+  private def createBasicParametersMap(command: String): Map[String, String] =
+    Map(CredentialsParameterManager.CredentialsFileOption -> CredentialsFilePath,
+      CredentialsParameterManager.SecretOption -> TestSecret,
+      ParameterParser.InputParameter.key -> command)
+
+  /**
+    * Helper function to check the credentials command config extractor. This
+    * extractor is executed on the passed in arguments.
+    *
+    * @param parameters the object with parsed parameters
+    * @param reader     an optional console reader
+    * @return a [[Future]] with the config and updated extraction context
+    */
+  private def extractCommandConfigParams(parameters: Parameters, reader: ConsoleReader = DummyConsoleReader):
+  Future[(CredentialsParameterManager.CommandConfig, ExtractionContext)] =
+    val context = ExtractorTestHelper.toExtractionContext(parameters, reader)
+    Future.fromTry(tryExtractor(CredentialsParameterManager.commandConfigExtractor, context))
+
+  /**
+    * Convenience function to run the credentials command config extractor on a
+    * map with single-valued parameters.
+    *
+    * @param paramsMap the map with parameters
+    * @param reader    an optional console reader
+    * @return a [[Future]] with the config and updated extraction context
+    */
+  private def extractCommandConfig(paramsMap: Map[String, String], reader: ConsoleReader = DummyConsoleReader):
+  Future[(CredentialsParameterManager.CommandConfig, ExtractionContext)] =
+    val params = ExtractorTestHelper.toParameters(ExtractorTestHelper.toParametersMap(paramsMap))
+    extractCommandConfigParams(params, reader)
+end CredentialsParameterManagerSpec
+
+/**
+  * Test class for [[CredentialsParameterManager]].
+  */
+class CredentialsParameterManagerSpec extends AsyncFlatSpec with Matchers with MockitoSugar:
+
+  import CredentialsParameterManagerSpec.*
+
+  /**
+    * Expects that the given [[Future]] fails with a
+    * [[ParameterExtractionException]] whose message contains all the provided
+    * message parts.
+    *
+    * @param msgParts the expected message parts
+    * @param future   the [[Future]] to check
+    * @return the result of the evaluation
+    */
+  private def recoverToSucceededIfParamsException(msgParts: String*)(future: Future[Any]): Future[Assertion] =
+    recoverToExceptionIf[ParameterExtractionException](future) map : exception =>
+      val message = exception.failures.mkString(" ")
+      forEvery(msgParts):
+        message should include(_)
+
+  "CredentialsParameterManager" should "extract a valid config for the list command" in :
+    val params = createBasicParametersMap(CredentialsParameterManager.CommandListCredentials)
+
+    extractCommandConfig(params) map : (config, ctx) =>
+      ExtractorTestHelper.accessedKeys(ctx) should contain only(
+        CredentialsParameterManager.CredentialsFileOption,
+        CredentialsParameterManager.SecretOption,
+        ParameterParser.InputParameter.key,
+        CliActorSystemLifeCycle.FileOption
+      )
+      config match
+        case c: CredentialsParameterManager.ListCommandConfig =>
+          c.credentialsFilePath.toString should be(CredentialsFilePath)
+          c.secret.secret should be(TestSecret)
+  //case c => fail("Unexpected configuration: " + c)
+
+  it should "report missing mandatory basic options" in :
+    val args = Map(ParameterParser.InputParameter.key -> CredentialsParameterManager.CommandListCredentials)
+
+    recoverToSucceededIfParamsException(CredentialsParameterManager.CredentialsFileOption):
+      extractCommandConfig(args)
+
+  it should "report a missing command" in :
+    val args = createBasicParametersMap("foo") - ParameterParser.InputParameter.key
+    recoverToSucceededIfParamsException(CredentialsParameterManager.CommandOption):
+      extractCommandConfig(args)
+
+  it should "read the secret for the credentials file from the console if required" in :
+    val reader = mock[ConsoleReader]
+    when(reader.readOption(CredentialsParameterManager.SecretOption, password = true))
+      .thenReturn(TestSecret)
+    val args = createBasicParametersMap(CredentialsParameterManager.CommandListCredentials) -
+      CredentialsParameterManager.SecretOption
+
+    extractCommandConfig(args, reader) map : (config, _) =>
+      config.secret.secret should be(TestSecret)
