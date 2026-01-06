984ef1509203cb3c2400682ad9420e536249dafd
Oliver Heger <oliver.heger@oliver-heger.de>
2026-01-06 21:10:54 +0100

    CredentialsCommands: Added a function to add a credential.
    This function adds a credential with a secret value to a credentials
    file or overrides the value of an existing credential.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- src/test/scala/com/github/sync/cli/credentials/CredentialsCommandsImplSpec.scala
+++ src/test/scala/com/github/sync/cli/credentials/CredentialsCommandsImplSpec.scala
@@ -18,7 +18,10 @@ package com.github.sync.cli.credentials
 
 import com.github.cloudfiles.core.http.Secret
 import com.github.sync.auth.{CredentialsService, CredentialsServiceImpl}
+import org.apache.pekko.Done
 import org.apache.pekko.actor.ActorSystem
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.{any, eq as argEq}
 import org.mockito.Mockito.*
 import org.scalatest.Inspectors.forEvery
 import org.scalatest.flatspec.AsyncFlatSpec
@@ -26,7 +29,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 
 import java.nio.file.Paths
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object CredentialsCommandsImplSpec:
   /** Test path to the credentials file. */
@@ -45,6 +48,38 @@ class CredentialsCommandsImplSpec extends AsyncFlatSpec with Matchers with Mocki
 
   /** A mock actor system used for service invocations. */
   given mockActorSystem: ActorSystem = mock
+
+  /**
+    * Prepares the given mock for a credentials service to expect an invocation
+    * of the ''storeCredentials()'' operation.
+    *
+    * @param service the mock service
+    */
+  private def expectUpdatedCredentials(service: CredentialsService[CredentialsServiceImpl.CredentialEntry]): Unit =
+    when(service.storeCredentials(argEq(CredentialsFilePath), argEq(TestSecret), any())(using any(), any()))
+      .thenReturn(Future.successful(Done))
+
+  /**
+    * Verifies that the given mock for a credentials service was called to
+    * store updated credentials and returns the credentials passed to this
+    * invocation.
+    *
+    * @param service the mock service
+    * @return the credentials that have been stored as a map
+    */
+  private def fetchUpdatedCredentials(service: CredentialsService[CredentialsServiceImpl.CredentialEntry]):
+  Map[String, String] =
+    given ActorSystem = argEq(mockActorSystem)
+
+    given ExecutionContext = any()
+
+    val captCredentials = ArgumentCaptor.forClass(classOf[Iterable[CredentialsServiceImpl.CredentialEntry]])
+    verify(service).storeCredentials(
+      argEq(CredentialsFilePath),
+      argEq(TestSecret),
+      captCredentials.capture()
+    )
+    captCredentials.getValue.map(e => e.key -> e.value.secret).toMap
 
   "A CredentialsCommandsImpl" should "list the keys of credentials from an existing file" in :
     val credentialKeys = List("cred1", "anotherCred", "testCred", "credit")
@@ -69,3 +104,43 @@ class CredentialsCommandsImplSpec extends AsyncFlatSpec with Matchers with Mocki
       commands.listCredentials(CredentialsFilePath, TestSecret)
     .map: actualException =>
       actualException should be(exception)
+
+  it should "add a new credential to a file" in :
+    val CredentialKey = "newKey"
+    val CredentialValue = Secret("newSecret")
+    val credentialsService = mock[CredentialsService[CredentialsServiceImpl.CredentialEntry]]
+    when(credentialsService.loadCredentialsOrEmpty(CredentialsFilePath, TestSecret))
+      .thenReturn(Future.successful(Nil))
+    expectUpdatedCredentials(credentialsService)
+
+    val commands = new CredentialsCommandsImpl(credentialsService)
+    commands.addCredential(CredentialsFilePath, TestSecret, CredentialKey, CredentialValue) map : result =>
+      val updatedCredentials = fetchUpdatedCredentials(credentialsService)
+      updatedCredentials should have size 1
+      updatedCredentials(CredentialKey) should be(CredentialValue.secret)
+      result should include(CredentialsFilePath.toString)
+      result should include(s"'$CredentialKey' was added")
+      result should include("contains 1 credential(s)")
+
+  it should "override the value of a credential" in :
+    val CredentialKey = "existingKey"
+    val CredentialValue = Secret("changedSecret")
+    val existingCredentials = List(
+      CredentialsServiceImpl.CredentialEntry("someKey", Secret("someSecret")),
+      CredentialsServiceImpl.CredentialEntry(CredentialKey, Secret("oldValue")),
+      CredentialsServiceImpl.CredentialEntry("someOtherKey", Secret("someOtherSecret"))
+    )
+    val credentialsService = mock[CredentialsService[CredentialsServiceImpl.CredentialEntry]]
+    when(credentialsService.loadCredentialsOrEmpty(CredentialsFilePath, TestSecret))
+      .thenReturn(Future.successful(existingCredentials))
+    expectUpdatedCredentials(credentialsService)
+
+    val commands = new CredentialsCommandsImpl(credentialsService)
+    commands.addCredential(CredentialsFilePath, TestSecret, CredentialKey, CredentialValue) map : result =>
+      val updatedCredentials = fetchUpdatedCredentials(credentialsService)
+      updatedCredentials should have size 3
+      updatedCredentials(CredentialKey) should be(CredentialValue.secret)
+      updatedCredentials("someKey") should be("someSecret")
+      result should include(CredentialsFilePath.toString)
+      result should include(s"'$CredentialKey' was replaced")
+      result should include("contains 3 credential(s)")
